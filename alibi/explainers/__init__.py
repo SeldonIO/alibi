887fcfcb5262b17cdd2d314afdeaf36ae1d318b8
giovac73 <gv@seldon.io>
2019-03-19 16:33:35 +0000

    classes CounterFactualRandomSearch and CounterFactualAdversarialSearch added

--- alibi/explainers/__init__.py
+++ alibi/explainers/__init__.py
@@ -4,6 +4,10 @@ The 'alibi.explainers' module includes feature importance, counterfactual and an
 
 from .anchor.anchor_tabular import AnchorTabular
 from .anchor.anchor_text import AnchorText
+from .counterfactual.counterfactuals import CounterFactualAdversarialSearch
+from .counterfactual.counterfactuals import CounterFactualRandomSearch
 
 __all__ = ["AnchorTabular",
-           "AnchorText"]
+           "AnchorText",
+           "CounterFactualRandomSearch",
+           "CounterFactualAdversarialSearch"]

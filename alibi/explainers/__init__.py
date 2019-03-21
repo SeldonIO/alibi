45f62fe53da82a0c432673f6ec560bd152b3e969
arnaudvl <avl@seldon.io>
2019-03-21 15:11:21 +0000

    add anchor image (#20)
    * add anchor image
    
    * add matplotlib version
    
    * add discretizer
    
    * update test anchor
    
    * add tf requirement
    
    * update tf version
    
    * update discretizer
    
    * Add more granular types to discretizer.py

--- alibi/explainers/__init__.py
+++ alibi/explainers/__init__.py
@@ -4,6 +4,8 @@ The 'alibi.explainers' module includes feature importance, counterfactual and an
 
 from .anchor.anchor_tabular import AnchorTabular
 from .anchor.anchor_text import AnchorText
+from .anchor.anchor_image import AnchorImage
 
 __all__ = ["AnchorTabular",
-           "AnchorText"]
+           "AnchorText",
+           "AnchorImage"]

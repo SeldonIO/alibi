8da5e771c460d0a80c90e738fd82cc3f8a6305a9
Janis Klaise <jk@seldon.io>
2019-04-10 11:12:36 +0100

    Implements the class-specific predicition function used in optimization

---
+++ alibi/explainers/counterfactual/counterfactual.py
@@ -0,0 +1,51 @@
+import numpy as np
+from typing import Callable, Tuple, Union
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def _define_func(predict_fn: Callable,
+                 pred_class: int,
+                 target_class: Union[str, int] = 'same') -> Tuple[Callable, Union[str, int]]:
+    """
+    Define the class-specific prediction function to be used in the optimization.
+
+    Parameters
+    ----------
+    predict_fn
+        Classifier prediction function
+    pred_class
+        Predicted class of the instance to be explained
+    target_class
+        Target class of the explanation, one of 'same', 'other' or an integer class
+
+    Returns
+    -------
+        Class-specific prediction function and the target class used.
+
+    """
+    if target_class == 'other':
+
+        def func(X):
+            probas = predict_fn(X)
+            sorted = np.argsort(-probas)  # class indices in decreasing order of probability
+
+            # take highest probability class different from class predicted for X
+            if sorted[0, 0] == pred_class:
+                target_class = sorted[0, 1]
+            else:
+                target_class = sorted[0, 0]
+
+            logger.debug('Current best target class: %s', target_class)
+            return predict_fn(X)[:, target_class]
+
+        return func, target_class
+
+    elif target_class == 'same':
+        target_class = pred_class
+
+    def func(X):  # type: ignore
+        return predict_fn(X)[:, target_class]
+
+    return func, target_class
---
+++ alibi/explainers/counterfactual/tests/test_counterfactual.py
@@ -0,0 +1,38 @@
+import pytest
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+
+from alibi.explainers.counterfactual.counterfactual  import _define_func
+
+
+@pytest.fixture
+def logistic_iris():
+    X, y = load_iris(return_X_y=True)
+    lr = LogisticRegression().fit(X, y)
+    return X, y, lr
+
+
+@pytest.mark.parametrize('target_class', ['other', 'same', 0, 1, 2])
+def test_define_func(logistic_iris, target_class):
+    X, y, model = logistic_iris
+
+    x = X[0].reshape(1, -1)
+    predict_fn = model.predict_proba
+    probas = predict_fn(x)
+    pred_class = probas.argmax(axis=1)[0]
+    pred_prob = probas[:, pred_class][0]
+
+    func, target = _define_func(predict_fn, pred_class, target_class)
+
+    if target_class == 'same':
+        assert target == pred_class
+        assert func(x) == pred_prob
+    elif isinstance(target_class, int):
+        assert target == target_class
+        assert func(x) == probas[:, target]
+    elif target_class == 'other':
+        assert target == 'other'
+        # highest probability different to the class of x
+        ix2 = np.argsort(-probas)[:, 1]
+        assert func(x) == probas[:, ix2]

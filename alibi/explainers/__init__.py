d360b03fd6958c0525bef18089e39a1d31740843
Janis Klaise <jk@seldon.io>
2019-04-17 10:18:23 +0100

    Expose counterfactual at alibi.explainers level

--- alibi/explainers/__init__.py
+++ alibi/explainers/__init__.py
@@ -6,8 +6,10 @@ from .anchor.anchor_tabular import AnchorTabular
 from .anchor.anchor_text import AnchorText
 from .anchor.anchor_image import AnchorImage
 from .cem.cem import CEM
+from .counterfactual.counterfactual import CounterFactual
 
 __all__ = ["AnchorTabular",
            "AnchorText",
            "AnchorImage",
-           "CEM"]
+           "CEM",
+           "CounterFactual"]

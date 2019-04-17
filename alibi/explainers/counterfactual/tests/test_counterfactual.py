0c3eb0107ddb2ef82dceff91fded43bb1700d423
Janis Klaise <jk@seldon.io>
2019-04-17 10:21:15 +0100

    Implements Wachter algorithm using tensorflow for loss minimization

--- alibi/explainers/counterfactual/tests/test_counterfactual.py
+++ alibi/explainers/counterfactual/tests/test_counterfactual.py
@@ -4,8 +4,10 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 from scipy.spatial.distance import cityblock
+import tensorflow as tf
 
 from alibi.explainers.counterfactual.counterfactual import _define_func, num_grad, get_wachter_grads
+from alibi.explainers import CounterFactual
 
 
 @pytest.fixture
@@ -13,6 +15,17 @@ def logistic_iris():
     X, y = load_iris(return_X_y=True)
     lr = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=200).fit(X, y)
     return X, y, lr
+
+
+@pytest.fixture
+def iris_explainer(logistic_iris):
+    X, y, lr = logistic_iris
+    predict_fn = lambda x: lr.predict_proba(x.reshape(1, -1))
+    sess = tf.Session()
+
+    cf_explainer = CounterFactual(sess=sess, predict_fn=predict_fn, data_shape=(4,), lam_init=1000, max_iter=2000)
+
+    return cf_explainer
 
 
 @pytest.mark.parametrize('target_class', ['other', 'same', 0, 1, 2])
@@ -77,7 +90,32 @@ def test_get_wachter_grads(logistic_iris):
     pred_class = probas.argmax()
     func, target = _define_func(predict_fn, pred_class, 'same')
 
-    grad_loss = get_wachter_grads(X_current=x, predict_class_fn=func, distance_fn=cityblock,
-                                  X_test=x, target_proba=0.1, lam=1)
+    loss, grad_loss = get_wachter_grads(X_current=x, predict_class_fn=func, distance_fn=cityblock,
+                                        X_test=x, target_proba=0.1, lam=1)
 
+    assert loss.shape == (1,)
     assert grad_loss.shape == x.shape
+
+
+def test_cf_explainer_iris(iris_explainer, logistic_iris):
+    X, y, lr = logistic_iris
+    x = X[0]
+    probas = iris_explainer.predict_fn(x)
+    pred_class = probas.argmax()
+
+    assert iris_explainer.data_shape == (4,)
+
+    # test explanation
+    exp = iris_explainer.explain(x)
+    x_cf = exp['X_cf']
+    assert x.shape == x_cf.shape
+
+    probas_cf = iris_explainer.predict_fn(x_cf)
+    pred_class_cf = probas_cf.argmax()
+
+    # check if 'other' class condition is met
+    assert pred_class != pred_class_cf
+
+    # check if probability is within tolerance
+    assert np.abs((iris_explainer.predict_class_fn(x_cf) - iris_explainer.target_proba)) <= iris_explainer.tol
+

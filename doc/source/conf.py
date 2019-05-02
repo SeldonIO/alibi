85e1e0c40a6a5639bdb93fe1d1beea0dd12f63eb
Janis Klaise <jk@seldon.io>
2019-05-02 17:07:56 +0100

    Add tensorflow to doc generation for type information

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,7 +71,7 @@ apidoc_extra_args = ['-d 6']
 
 # mock imports
 autodoc_mock_imports = ['pandas', 'sklearn', 'spacy', 'skimage', 'requests',
-                        'cv2', 'bs4', 'keras', 'tensorflow', 'seaborn']
+                        'cv2', 'bs4', 'keras', 'seaborn']
 
 # Napoleon settings
 napoleon_google_docstring = True

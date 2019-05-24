161cbf1d7fec9f5e38733ff1ea34ad60eabfef8a
Janis Klaise <jk@seldon.io>
2019-05-24 10:45:08 +0100

    Fix typo in exporting methods

--- alibi/explainers/__init__.py
+++ alibi/explainers/__init__.py
@@ -13,5 +13,5 @@ __all__ = ["AnchorTabular",
            "AnchorText",
            "AnchorImage",
            "CEM",
-           "CounterFactualProto"]
+           "CounterFactualProto",
            "CounterFactual"]

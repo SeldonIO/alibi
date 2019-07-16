5b2ca320c0a017da554fad5a5f547bde08a3b19e
Janis Klaise <jk@seldon.io>
2019-07-16 17:44:28 +0100

    Initial get_category_map utility function

--- alibi/utils/data.py
+++ alibi/utils/data.py
@@ -1,3 +1,8 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from typing import Dict
+
+
 class Bunch(dict):
     """
     Container object for internal datasets
@@ -18,3 +23,23 @@ class Bunch(dict):
             return self[key]
         except KeyError:
             raise AttributeError(key)
+
+
+def get_category_map(data: pd.DataFrame,
+                     categorical_columns: list = None) -> Dict[int, list]:
+    # TODO: support passing np.ndarray
+    assert data.ndim == 2, 'Expected 2-dimensional dataframe'
+
+    n_features = data.shape[1]
+    if categorical_columns is None:
+        # infer categorical columns
+        categorical_columns = [i for i in range(n_features) if data.iloc[:, i].dtype == 'O']
+
+    # create the map
+    category_map = dict.fromkeys(categorical_columns)
+    for col in categorical_columns:
+        le = LabelEncoder()
+        _ = le.fit_transform(data.iloc[:, col])
+        category_map[col] = list(le.classes_)
+
+    return category_map

8c5b994ea72116f1758fcf38aa62da71276d60c7
Arnaud Van Looveren <avl@seldon.io>
2019-09-19 16:07:47 +0100

    update dist tests

--- alibi/utils/tests/test_distance.py
+++ alibi/utils/tests/test_distance.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.spatial.distance import cityblock
 from itertools import product
 import pytest
-from alibi.utils.distance import abdm, cityblock_batch, mvdm
+from alibi.utils.distance import abdm, cityblock_batch, mvdm, multidim_scaling
 
 dims = np.array([1, 10, 50])
 shapes = list(product(dims, dims))
@@ -53,5 +53,45 @@ def test_abdm_mvdm(cats_and_labels):
     else:
         d_pair = mvdm(X, y, cat_vars)
     assert list(d_pair.keys()) == list(cat_vars.keys())
-    assert d_pair[0].shape == (cat_vars[0], cat_vars[0])
-    assert d_pair[0].min() >= 0
+    for k, v in d_pair.items():
+        assert v.shape == (cat_vars[k], cat_vars[k])
+        assert v.min() >= 0
+
+
+Xy = (4, 2, 100, 5)
+idx = np.where([t == Xy for t in tests])[0].item()
+feature_range = ((np.ones((1, 5)) * -1).astype(np.float32),
+                 (np.ones((1, 5))).astype(np.float32))
+
+
+@pytest.mark.parametrize('cats_and_labels,rng,update_rng,center',
+                         [(idx, feature_range, False, False),
+                          (idx, feature_range, True, False),
+                          (idx, feature_range, False, True),
+                          (idx, feature_range, True, True)],
+                         indirect=['cats_and_labels'])
+def test_multidim_scaling(cats_and_labels, rng, update_rng, center):
+    # compute pairwise distance
+    X, y = cats_and_labels
+    n_cols = X.shape[1]
+    cat_vars = {i: None for i in range(n_cols)}
+    d_pair = abdm(X, cat_vars)
+
+    # apply multidimensional scaling
+    d_abs, new_rng = multidim_scaling(d_pair,
+                                      feature_range=rng,
+                                      update_feature_range=update_rng,
+                                      center=center
+                                      )
+    assert list(d_abs.keys()) == list(cat_vars.keys())
+    if update_rng:
+        assert (new_rng[0] != rng[0]).any()
+        assert (new_rng[1] != rng[1]).any()
+    else:
+        assert (new_rng[0] == rng[0]).all()
+        assert (new_rng[1] == rng[1]).all()
+
+    for k, v in d_abs.items():
+        assert v.shape[0] == d_pair[k].shape[0]
+        if center:
+            assert (v.max() + v.min()) - (rng[1][0, k] + rng[0][0, k]) < 1e-5

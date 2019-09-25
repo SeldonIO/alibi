9159069437ce3087b8cf00f68d4e8d318c7a8e40
arnaudvl <avl@seldon.io>
2019-09-25 09:47:55 +0100

    Categorical Counterfactuals (#144)
    * update distance metrics
    
    * add util fn for distance, mapping, custom tf and data transforms
    
    * update distance fn
    
    * stabilize dist measures
    
    * adjust cfproto code for categorical variables
    
    * add example notebook and bugfix of kdtree dim
    
    * add ordinal example notebook
    
    * add type annotations
    
    * update test cfproto
    
    * update cfproto test and fix bug mapping
    
    * add test mapping
    
    * remove unused import
    
    * update mapping test and data type
    
    * add test for abdm and mvdm distance
    
    * update dist tests
    
    * extend cfproto test
    
    * expand cfproto docs
    
    * update cfproto docs
    
    * Insert missing link to docs
    
    * Make example reproducible
    
    * Update example
    
    * Adjust mapping docstring
    
    * Standardize setting seeds for the other example
    
    * Add comment indicating KL divergence
    
    * Change default argument to None

--- alibi/utils/tf.py
+++ alibi/utils/tf.py
@@ -57,3 +57,48 @@ def _check_keras_or_tf(predict_fn: Union[Callable, tf.keras.Model, 'keras.Model'
         is_keras = False
         tfsess = tf.keras.backend.get_session()
         return is_model, is_keras, tfsess
+
+
+@tf.custom_gradient
+def argmin_grad(x, y):
+    abs_diff = tf.abs(tf.subtract(x, y))
+    argmin = tf.cast(tf.argmin(abs_diff), tf.float32)
+
+    def grad(dy):
+        """ Let gradients pass through. """
+        return dy, None
+
+    return argmin, grad
+
+
+@tf.custom_gradient
+def one_hot_grad(x, y):
+    cat_ohe = tf.one_hot(indices=tf.cast(x, tf.int32), depth=tf.shape(y)[0])
+
+    def grad(dy):
+        """ Let gradients pass through. """
+        return tf.reduce_sum(dy), None
+
+    return cat_ohe, grad
+
+
+@tf.custom_gradient
+def argmax_grad(x):
+    argmax = tf.argmax(x)
+
+    def grad(dy):
+        """ Let gradients pass through. """
+        return tf.ones(tf.shape(x)) * dy
+
+    return argmax, grad
+
+
+@tf.custom_gradient
+def round_grad(x):
+    idx = tf.cast(tf.round(x), tf.int32)
+
+    def grad(dy):
+        """ Let gradients pass through. """
+        return dy
+
+    return idx, grad

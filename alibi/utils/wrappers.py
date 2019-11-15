03bc0daf1b3b2a678884208e06ec305c4bb25aee
Alex Coca <alexcoca23@yahoo.co.uk>
2019-11-15 17:41:25 +0000

    feat: added sklearn classifier wrapper

---
+++ alibi/utils/wrappers.py
@@ -0,0 +1,13 @@
+class Predictor(object):
+    """ Implements a simple wrapper around an sklearn model that can be pickled."""
+    def __init__(self, clf, preprocessor=None):
+
+        if not hasattr(clf, 'predict'):
+            raise AttributeError('Classifier object is expected to have a predict method!')
+
+        self.predict_fcn = clf.predict
+        self.preprocessor = preprocessor
+
+    def __call__(self, x):
+        if self.preprocessor:
+            return self.predict_fcn(self.preprocessor.transform(x))

f67382221216ab78a062b165e09d076d61b45b50
giovac73 <giova.physics@gmail.com>
2020-02-27 16:51:21 +0000

    Added NoiseTunnelPytorch class

--- alibi/explainers/integrated_gradients.py
+++ alibi/explainers/integrated_gradients.py
@@ -4,13 +4,8 @@ import tensorflow as tf
 import logging
 
 from captum.attr import IntegratedGradients, LayerConductance, NeuronConductance
+from captum.attr import NoiseTunnel
 import torch
-#import torch.nn as nn
-import pandas as pd
-from scipy import stats
-
-from alibi.utils.gradients import num_grad_batch
-from alibi.utils.tf import _check_keras_or_tf
 
 if TYPE_CHECKING:  # pragma: no cover
     import keras  # noqa
@@ -22,26 +17,25 @@ def _define_model(model):
     return model
 
 
-class IntegratedGradients:
+class IntGradsPytorch(object):
 
-    def __init__(self, model, **kwargs):
+    def __init__(self, model):
         """
 
         Parameters
         ----------
         model
-        kwargs
         """
-        self.ig = IntegratedGradients(model, **kwargs)
+        self.am = IntegratedGradients(model)
 
     def explain(self, X,
-                baselines = None,
-                target = None,
-                additional_forward_args = None,
-                n_steps = 50,
-                method = "gausslegendre",
-                internal_batch_size = None,
-                return_convergence_delta = False
+                baselines=None,
+                target=None,
+                additional_forward_args=None,
+                n_steps=50,
+                method="gausslegendre",
+                internal_batch_size=None,
+                return_convergence_delta=False
                 ):
         """
 
@@ -62,8 +56,9 @@ class IntegratedGradients:
         """
         test_input_tensor = torch.from_numpy(X).type(torch.FloatTensor)
         test_input_tensor.requires_grad_()
+        setattr(self.am, 'return_convergence_delta', return_convergence_delta)
         if return_convergence_delta:
-            attr, delta = self.ig.attribute(test_input_tensor,
+            attr, delta = self.am.attribute(test_input_tensor,
                                             baselines=baselines,
                                             target=target,
                                             additional_forward_args=additional_forward_args,
@@ -72,7 +67,7 @@ class IntegratedGradients:
                                             internal_batch_size=internal_batch_size,
                                             return_convergence_delta=return_convergence_delta)
         else:
-            attr = self.ig.attribute(test_input_tensor,
+            attr = self.am.attribute(test_input_tensor,
                                      baselines=baselines,
                                      target=target,
                                      additional_forward_args=additional_forward_args,
@@ -80,6 +75,43 @@ class IntegratedGradients:
                                      method=method,
                                      internal_batch_size=internal_batch_size,
                                      return_convergence_delta=return_convergence_delta)
+
+        attr = attr.detach().numpy()
+
+        return attr
+
+
+class NoiseTunnelPytorch(object):
+
+    def __init__(self, pytorch_explainer):
+        if hasattr(pytorch_explainer, 'am'):
+            self.am = pytorch_explainer.am
+            self.nt = NoiseTunnel(self.am)
+        else:
+            raise AttributeError('explainer must have am (attribute method) attribute')
+
+    def explain(self, X,
+                nt_type="smoothgrad",
+                n_samples=5,
+                stdevs=1.0,
+                draw_baseline_from_distrib=False,
+                **kwargs):
+
+        inputs = torch.from_numpy(X).type(torch.FloatTensor)
+        if self.am.return_convergence_delta:
+            attr, delta = self.nt.attribute(inputs,
+                                            nt_type=nt_type,
+                                            n_samples=n_samples,
+                                            stdevs=stdevs,
+                                            draw_baseline_from_distrib=draw_baseline_from_distrib,
+                                            **kwargs)
+        else:
+            attr = self.nt.attribute(inputs,
+                                     nt_type=nt_type,
+                                     n_samples=n_samples,
+                                     stdevs=stdevs,
+                                     draw_baseline_from_distrib=draw_baseline_from_distrib,
+                                     **kwargs)
 
         attr = attr.detach().numpy()
 

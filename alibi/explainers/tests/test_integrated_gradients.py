3e633386ded1544b2472d86e3ea7424092b80679
giovac73 <giova.physics@gmail.com>
2020-04-22 15:09:35 +0100

    cleaned up tests

--- alibi/explainers/tests/test_integrated_gradients.py
+++ alibi/explainers/tests/test_integrated_gradients.py
@@ -4,17 +4,62 @@ from alibi.explainers import IntegratedGradients
 from alibi.api.interfaces import Explanation
 
 
+def to_categorical(y, num_classes=None, dtype='float32'):
+    """Converts a class vector (integers) to binary class matrix.
+    E.g. for use with categorical_crossentropy.
+    # Arguments
+        y: class vector to be converted into a matrix
+            (integers from 0 to num_classes).
+        num_classes: total number of classes.
+        dtype: The data type expected by the input, as a string
+            (`float32`, `float64`, `int32`...)
+    # Returns
+        A binary matrix representation of the input. The classes axis
+        is placed last.
+    # Example
+    ```python
+    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
+    > labels
+    array([0, 2, 1, 2, 0])
+    # `to_categorical` converts this into a matrix with as many
+    # columns as there are classes. The number of rows
+    # stays the same.
+    > to_categorical(labels)
+    array([[ 1.,  0.,  0.],
+           [ 0.,  0.,  1.],
+           [ 0.,  1.,  0.],
+           [ 0.,  0.,  1.],
+           [ 1.,  0.,  0.]], dtype=float32)
+    ```
+    """
+
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+
+
+X = np.random.rand(100, 4)
+y = (X[:, 0] + X[:, 1] > 1).astype(int)
+y = to_categorical(y)
+X_train, y_train = X[:90, :], y[:90, :]
+X_test, y_test = X[90:, :], y[90:, :]
+test_labels = np.argmax(y_test, axis=1)
+
+
 @pytest.fixture(scope='module')
 def hacky_cnn(tensorflow):
     tf = tensorflow
     print(tf)
-
-    X = np.random.rand(100, 4)
-    y = (X[:, 0] + X[:, 1] > 1).astype(int)
-    y = tf.keras.utils.to_categorical(y)
-    X_train, y_train = X[:90, :], y[:90, :]
-    X_test, y_test = X[90:, :], y[90:, :]
-    test_labels = np.argmax(y_test, axis=1)
 
     inputs = tf.keras.Input(shape=(X.shape[1:]))
     print("TF executed eagerly:", tf.executing_eagerly())
@@ -36,7 +81,8 @@ def hacky_cnn(tensorflow):
               validation_data=(X_test, y_test)
               )
     print("TF executed eagerly:", tf.executing_eagerly())
-    return model, X_test, test_labels
+    return model
+
 
 @pytest.mark.parametrize('tensorflow', ('eager', ), indirect=True, ids='mode={}'.format)
 @pytest.mark.paramterize('hacky_cnn', (pytest.lazy_fixture('tensorflow'), ), ids='exp={}'.format)
@@ -49,7 +95,7 @@ def hacky_cnn(tensorflow):
 @pytest.mark.parametrize('rp', (True, False))
 @pytest.mark.parametrize('fn', (None, ['feat_{}'.format(i) for i in range(4)]))
 def test_integratedgradients(tensorflow, hacky_cnn, method, rcd, rp, fn):
-    model, X_test, test_labels = hacky_cnn
+    model = hacky_cnn
     ig = IntegratedGradients(model, n_steps=50, method=method, return_convergence_delta=rcd,
                              return_predictions=rp)
 
@@ -70,7 +116,6 @@ def test_integratedgradients(tensorflow, hacky_cnn, method, rcd, rp, fn):
         assert len(fn) == X_test.reshape(X_test.shape[0], -1).shape[1]
 
 
-#@pytest.mark.skip
 @pytest.mark.parametrize('tensorflow', ('eager', ), indirect=True, ids='mode={}'.format)
 @pytest.mark.paramterize('hacky_cnn', (pytest.lazy_fixture('tensorflow'), ), ids='exp={}'.format)
 @pytest.mark.parametrize('method', ('gausslegendre',
@@ -84,7 +129,7 @@ def test_integratedgradients(tensorflow, hacky_cnn, method, rcd, rp, fn):
 @pytest.mark.parametrize('layer_nb', (None, 1))
 def test_layer_integratedgradients(tensorflow, hacky_cnn, method, rcd, rp, fn, layer_nb):
 
-    model, X_test, test_labels = hacky_cnn
+    model = hacky_cnn
     if layer_nb is not None:
         layer = model.layers[layer_nb]
     else:

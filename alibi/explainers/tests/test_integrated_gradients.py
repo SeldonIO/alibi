17f0207bb99bc1177905c4137084c8de86b86ef2
Janis Klaise <jk@seldon.io>
2020-06-04 11:34:53 +0100

    Refactor tests
     - Removed duplicated keras model fixtures and use one parametrized
       instead
     - Meaningful names to test functions, dummy data and arguments

--- alibi/explainers/tests/test_integrated_gradients.py
+++ alibi/explainers/tests/test_integrated_gradients.py
@@ -3,215 +3,172 @@ import pytest
 from alibi.explainers import IntegratedGradients
 from alibi.api.interfaces import Explanation
 import tensorflow as tf
+
 tf.compat.v1.enable_eager_execution()
 
+# generate some dummy data
+N = 100
+N_TRAIN = 90
+N_FEATURES = 4
+X = np.random.rand(N, N_FEATURES)
+X_train, X_test = X[:N_TRAIN, :], X[N_TRAIN:, :]
 
-X = np.random.rand(100, 4)
-y_class = (X[:, 0] + X[:, 1] > 1).astype(int)
-y_reg = X[:, 0] + X[:, 1]
-y = tf.keras.utils.to_categorical(y_class)
+# regression labels
+y_regression = X[:, 0] + X[:, 1]
+y_train_regression = y_regression[:N_TRAIN]
 
-X_train_c, y_train_c, y_train_c_2 = X[:90, :], y[:90, :], y_class[:90]
-X_test_c, y_test_c, y_test_c_2 = X[90:, :], y[90:, :], y_class[90:]
+# classification labels
+y_classification_ordinal = (X[:, 0] + X[:, 1] > 1).astype(int)
+y_classification_categorical = tf.keras.utils.to_categorical(y_classification_ordinal)
 
-X_train_r, y_train_r = X[:90, :], y_reg[:90]
-X_test_r, y_test_r = X[90:, :], y_reg[90:]
-test_labels = np.argmax(y_test_c, axis=1)
+y_train_classification_ordinal = y_classification_ordinal[:N_TRAIN]
+y_train_classification_categorical = y_classification_categorical[:N_TRAIN, :]
+
+test_labels = y_classification_ordinal[N_TRAIN:]
+
+# integral method used shouldn't affect wrapper functionality
+INTEGRAL_METHODS = ['gausslegendre', 'riemann_middle']
 
 
-@pytest.fixture(scope='module')
-def hacky_class():
-
-    inputs = tf.keras.Input(shape=(X.shape[1:]))
-    x = tf.keras.layers.Dense(20, activation='linear')(inputs)
-    outputs = tf.keras.layers.Dense(2, activation='softmax')(x)
+@pytest.fixture()
+def ffn_model(request):
+    """
+    Simple feed-forward model with configurable data, loss funciton, output activation and dimension
+    """
+    config = request.param
+    inputs = tf.keras.Input(shape=config['X_train'].shape[1:])
+    x = tf.keras.layers.Dense(20, activation='relu')(inputs)
+    outputs = tf.keras.layers.Dense(config['output_dim'], activation=config['activation'])(x)
+    if config.get('squash_output', False):
+        outputs = tf.keras.layers.Reshape(())(outputs)
     model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    model.compile(loss=config['loss'],
+                  optimizer='adam')
 
-    # train model
-    model.fit(X_train_c,
-              y_train_c,
-              epochs=1,
-              batch_size=256,
-              verbose=0,
-              validation_data=(X_test_c, y_test_c)
-              )
-
-    return model
-
-
-@pytest.fixture(scope='module')
-def hacky_class_2():
-
-    inputs = tf.keras.Input(shape=(X.shape[1:]))
-    x = tf.keras.layers.Dense(20, activation='linear')(inputs)
-    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
-    model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-
-    # train model
-    model.fit(X_train_c,
-              y_train_c_2,
-              epochs=1,
-              batch_size=256,
-              verbose=0,
-              validation_data=(X_test_c, y_test_c_2)
-              )
-
-    return model
-
-
-@pytest.fixture(scope='module')
-def hacky_class_3():
-
-    inputs = tf.keras.Input(shape=(X.shape[1:]))
-    x = tf.keras.layers.Dense(20, activation='linear')(inputs)
-    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
-    outputs = tf.keras.layers.Reshape(())(outputs)
-    model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-
-    # train model
-    model.fit(X_train_c,
-              y_train_c_2,
-              epochs=1,
-              batch_size=256,
-              verbose=0,
-              validation_data=(X_test_c, y_test_c_2)
-              )
-
-    return model
-
-
-@pytest.fixture(scope='module')
-def hacky_reg():
-
-    inputs = tf.keras.Input(shape=(X.shape[1:]))
-    x = tf.keras.layers.Dense(20, activation='linear')(inputs)
-    outputs = tf.keras.layers.Dense(1, activation='linear')(x)
-    model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
-    model.compile(loss='mean_squared_error',
-                  optimizer='adam',
-                  metrics=['mean_squared_error'])
-
-    # train model
-    model.fit(X_train_r,
-              y_train_r,
-              epochs=1,
-              batch_size=256,
-              verbose=0,
-              validation_data=(X_test_r, y_test_r)
-              )
+    model.fit(config['X_train'], config['y_train'], epochs=1, batch_size=256, verbose=0)
 
     return model
 
 
 @pytest.mark.eager
-@pytest.mark.parametrize('method', ('gausslegendre',
-                                    "riemann_left",
-                                    "riemann_right",
-                                    "riemann_middle",
-                                    "riemann_trapezoid"))
-@pytest.mark.parametrize('rcd', (True, False))
-@pytest.mark.parametrize('rp', (True, False))
-@pytest.mark.parametrize('fn', (None, ['feat_{}'.format(i) for i in range(4)]))
-def test_integratedgradients(hacky_class, method, rcd, rp, fn):
-    model = hacky_class
+@pytest.mark.parametrize('ffn_model', [({'output_dim': 2,
+                                         'activation': 'softmax',
+                                         'loss': 'categorical_crossentropy',
+                                         'X_train': X_train,
+                                         'y_train': y_train_classification_categorical})], indirect=True)
+@pytest.mark.parametrize('method', INTEGRAL_METHODS, ids='method={}'.format)
+@pytest.mark.parametrize('return_convergence_delta', (True, False))
+@pytest.mark.parametrize('return_predictions', (True, False))
+@pytest.mark.parametrize('features_names', (None, ['feat_{}'.format(i) for i in range(4)]))
+def test_integrated_gradients_binary_classification(ffn_model, method, return_convergence_delta,
+                                                    return_predictions, features_names):
+    model = ffn_model
     ig = IntegratedGradients(model, n_steps=50, method=method)
 
-    explanations = ig.explain(X_test_c,
+    explanations = ig.explain(X_test,
                               baselines=None,
                               target=test_labels,
-                              features_names=fn,
-                              return_convergence_delta=rcd,
-                              return_predictions=rp)
+                              features_names=features_names,
+                              return_convergence_delta=return_convergence_delta,
+                              return_predictions=return_predictions)
 
     assert isinstance(explanations, Explanation)
-    assert explanations['data']['attributions'].shape == X_test_c.shape
-    if rcd:
+    assert explanations['data']['attributions'].shape == X_test.shape
+    if return_convergence_delta:
         assert 'deltas' in explanations['data'].keys()
-        assert explanations['data']['deltas'].shape[0] == X_test_c.shape[0]
-    if rp:
+        assert explanations['data']['deltas'].shape[0] == X_test.shape[0]
+    if return_predictions:
         assert 'predictions' in explanations['data'].keys()
-        assert explanations['data']['predictions'].shape[0] == X_test_c.shape[0]
-    if fn is not None:
-        assert len(fn) == X_test_c.reshape(X_test_c.shape[0], -1).shape[1]
+        assert explanations['data']['predictions'].shape[0] == X_test.shape[0]
+    if features_names is not None:
+        assert len(features_names) == X_test.reshape(X_test.shape[0], -1).shape[1]
 
 
 @pytest.mark.eager
-@pytest.mark.parametrize('method', ('gausslegendre',
-                                    "riemann_left"))
-@pytest.mark.parametrize('rcd', (True, False))
-@pytest.mark.parametrize('rp', (True, False))
-@pytest.mark.parametrize('fn', (None, ['feat_{}'.format(i) for i in range(4)]))
-def test_integratedgradients_2(hacky_class_2, method, rcd, rp, fn):
-    model = hacky_class_2
+@pytest.mark.parametrize('ffn_model', [({'output_dim': 1,
+                                         'activation': 'sigmoid',
+                                         'loss': 'binary_crossentropy',
+                                         'X_train': X_train,
+                                         'y_train': y_train_classification_ordinal})], indirect=True)
+@pytest.mark.parametrize('method', INTEGRAL_METHODS)
+@pytest.mark.parametrize('return_convergence_delta', (True, False))
+@pytest.mark.parametrize('return_predictions', (True, False))
+@pytest.mark.parametrize('features_names', (None, ['feat_{}'.format(i) for i in range(4)]))
+def test_integrated_gradients_binary_classification_single_output(ffn_model, method,
+                                                                  return_convergence_delta, return_predictions,
+                                                                  features_names):
+    model = ffn_model
     ig = IntegratedGradients(model, n_steps=50, method=method)
 
-    explanations = ig.explain(X_test_c,
+    explanations = ig.explain(X_test,
                               baselines=None,
                               target=test_labels,
-                              features_names=fn,
-                              return_convergence_delta=rcd,
-                              return_predictions=rp)
+                              features_names=features_names,
+                              return_convergence_delta=return_convergence_delta,
+                              return_predictions=return_predictions)
 
     assert isinstance(explanations, Explanation)
-    assert explanations['data']['attributions'].shape == X_test_c.shape
-    if rcd:
+    assert explanations['data']['attributions'].shape == X_test.shape
+    if return_convergence_delta:
         assert 'deltas' in explanations['data'].keys()
-        assert explanations['data']['deltas'].shape[0] == X_test_c.shape[0]
-    if rp:
+        assert explanations['data']['deltas'].shape[0] == X_test.shape[0]
+    if return_predictions:
         assert 'predictions' in explanations['data'].keys()
-        assert explanations['data']['predictions'].shape[0] == X_test_c.shape[0]
-    if fn is not None:
-        assert len(fn) == X_test_c.reshape(X_test_c.shape[0], -1).shape[1]
+        assert explanations['data']['predictions'].shape[0] == X_test.shape[0]
+    if features_names is not None:
+        assert len(features_names) == X_test.reshape(X_test.shape[0], -1).shape[1]
 
 
 @pytest.mark.eager
-@pytest.mark.parametrize('method', ('gausslegendre',
-                                    "riemann_left"))
-@pytest.mark.parametrize('rcd', (True, False))
-@pytest.mark.parametrize('rp', (True, False))
-@pytest.mark.parametrize('fn', (None, ['feat_{}'.format(i) for i in range(4)]))
-def test_integratedgradients_3(hacky_class_3, method, rcd, rp, fn):
-    model = hacky_class_3
+@pytest.mark.parametrize('ffn_model', [({'output_dim': 1,
+                                         'activation': 'sigmoid',
+                                         'loss': 'binary_crossentropy',
+                                         'X_train': X_train,
+                                         'y_train': y_train_classification_ordinal,
+                                         'squash_output': True})], indirect=True)
+@pytest.mark.parametrize('method', INTEGRAL_METHODS)
+@pytest.mark.parametrize('return_convergence_delta', (True, False))
+@pytest.mark.parametrize('return_predictions', (True, False))
+@pytest.mark.parametrize('features_names', (None, ['feat_{}'.format(i) for i in range(4)]))
+def test_integrated_gradients_binary_classification_single_output_squash_output(ffn_model, method,
+                                                                                return_convergence_delta,
+                                                                                return_predictions, features_names):
+    model = ffn_model
     ig = IntegratedGradients(model, n_steps=50, method=method)
 
-    explanations = ig.explain(X_test_c,
+    explanations = ig.explain(X_test,
                               baselines=None,
                               target=test_labels,
-                              features_names=fn,
-                              return_convergence_delta=rcd,
-                              return_predictions=rp)
+                              features_names=features_names,
+                              return_convergence_delta=return_convergence_delta,
+                              return_predictions=return_predictions)
 
     assert isinstance(explanations, Explanation)
-    assert explanations['data']['attributions'].shape == X_test_c.shape
-    if rcd:
+    assert explanations['data']['attributions'].shape == X_test.shape
+    if return_convergence_delta:
         assert 'deltas' in explanations['data'].keys()
-        assert explanations['data']['deltas'].shape[0] == X_test_c.shape[0]
-    if rp:
+        assert explanations['data']['deltas'].shape[0] == X_test.shape[0]
+    if return_predictions:
         assert 'predictions' in explanations['data'].keys()
-        assert explanations['data']['predictions'].shape[0] == X_test_c.shape[0]
-    if fn is not None:
-        assert len(fn) == X_test_c.reshape(X_test_c.shape[0], -1).shape[1]
+        assert explanations['data']['predictions'].shape[0] == X_test.shape[0]
+    if features_names is not None:
+        assert len(features_names) == X_test.reshape(X_test.shape[0], -1).shape[1]
 
 
 @pytest.mark.eager
-@pytest.mark.parametrize('method', ('gausslegendre',
-                                    "riemann_left"))
-@pytest.mark.parametrize('rcd', (True, False))
-@pytest.mark.parametrize('rp', (True, False))
-@pytest.mark.parametrize('fn', (None, ['feat_{}'.format(i) for i in range(4)]))
+@pytest.mark.parametrize('ffn_model', [({'output_dim': 2,
+                                         'activation': 'softmax',
+                                         'loss': 'categorical_crossentropy',
+                                         'X_train': X_train,
+                                         'y_train': y_train_classification_categorical})], indirect=True)
+@pytest.mark.parametrize('method', INTEGRAL_METHODS)
+@pytest.mark.parametrize('return_convergence_delta', (True, False))
+@pytest.mark.parametrize('return_predictions', (True, False))
+@pytest.mark.parametrize('features_names', (None, ['feat_{}'.format(i) for i in range(4)]))
 @pytest.mark.parametrize('layer_nb', (None, 1))
-def test_layer_integratedgradients(hacky_class, method, rcd, rp, fn, layer_nb):
-
-    model = hacky_class
+def test_integrated_gradients_binary_classification_layer(ffn_model, method, return_convergence_delta,
+                                                          return_predictions, features_names, layer_nb):
+    model = ffn_model
     if layer_nb is not None:
         layer = model.layers[layer_nb]
     else:
@@ -220,53 +177,58 @@ def test_layer_integratedgradients(hacky_class, method, rcd, rp, fn, layer_nb):
     ig = IntegratedGradients(model, layer=layer,
                              n_steps=50, method=method)
 
-    explanations = ig.explain(X_test_c,
+    explanations = ig.explain(X_test,
                               baselines=None,
                               target=test_labels,
-                              features_names=fn,
-                              return_convergence_delta=rcd,
-                              return_predictions=rp)
+                              features_names=features_names,
+                              return_convergence_delta=return_convergence_delta,
+                              return_predictions=return_predictions)
 
     assert isinstance(explanations, Explanation)
     if layer is not None:
-        layer_out = layer(X_test_c).numpy()
+        layer_out = layer(X_test).numpy()
         assert explanations['data']['attributions'].shape == layer_out.shape
     else:
-        assert explanations['data']['attributions'].shape == X_test_c.shape
-    if rcd:
+        assert explanations['data']['attributions'].shape == X_test.shape
+    if return_convergence_delta:
         assert 'deltas' in explanations['data'].keys()
-        assert explanations['data']['deltas'].shape[0] == X_test_c.shape[0]
-    if rp:
+        assert explanations['data']['deltas'].shape[0] == X_test.shape[0]
+    if return_predictions:
         assert 'predictions' in explanations['data'].keys()
-        assert explanations['data']['predictions'].shape[0] == X_test_c.shape[0]
-    if fn is not None:
-        assert len(fn) == X_test_c.reshape(X_test_c.shape[0], -1).shape[1]
+        assert explanations['data']['predictions'].shape[0] == X_test.shape[0]
+    if features_names is not None:
+        assert len(features_names) == X_test.reshape(X_test.shape[0], -1).shape[1]
 
 
 @pytest.mark.eager
-@pytest.mark.parametrize('method', ('gausslegendre',
-                                    "riemann_left"))
-@pytest.mark.parametrize('rcd', (True, False))
-@pytest.mark.parametrize('rp', (True, False))
-@pytest.mark.parametrize('fn', (None, ['feat_{}'.format(i) for i in range(4)]))
-def test_integratedgradients_reg(hacky_reg, method, rcd, rp, fn):
-    model = hacky_reg
+@pytest.mark.parametrize('ffn_model', [({'output_dim': 1,
+                                         'activation': 'linear',
+                                         'loss': 'mean_squared_error',
+                                         'X_train': X_train,
+                                         'y_train': y_train_regression})], indirect=True)
+@pytest.mark.parametrize('method', INTEGRAL_METHODS)
+@pytest.mark.parametrize('return_convergence_delta', (True, False))
+@pytest.mark.parametrize('return_predictions', (True, False))
+@pytest.mark.parametrize('features_names', (None, ['feat_{}'.format(i) for i in range(4)]))
+def test_integrated_gradients_regression(ffn_model, method, return_convergence_delta, return_predictions,
+                                         features_names):
+    model = ffn_model
     ig = IntegratedGradients(model, n_steps=50, method=method)
 
-    explanations = ig.explain(X_test_r,
+    explanations = ig.explain(X_test,
                               baselines=None,
                               target=None,
-                              features_names=fn,
-                              return_convergence_delta=rcd,
-                              return_predictions=rp)
+                              features_names=features_names,
+                              return_convergence_delta=return_convergence_delta,
+                              return_predictions=return_predictions)
 
     assert isinstance(explanations, Explanation)
-    assert explanations['data']['attributions'].shape == X_test_r.shape
-    if rcd:
+    assert explanations['data']['attributions'].shape == X_test.shape
+    if return_convergence_delta:
         assert 'deltas' in explanations['data'].keys()
-        assert explanations['data']['deltas'].shape[0] == X_test_r.shape[0]
-    if rp:
+        assert explanations['data']['deltas'].shape[0] == X_test.shape[0]
+    if return_predictions:
         assert 'predictions' in explanations['data'].keys()
-        assert explanations['data']['predictions'].shape[0] == X_test_r.shape[0]
-    if fn is not None:
-        assert len(fn) == X_test_r.reshape(X_test_r.shape[0], -1).shape[1]
+        assert explanations['data']['predictions'].shape[0] == X_test.shape[0]
+    if features_names is not None:
+        assert len(features_names) == X_test.reshape(X_test.shape[0], -1).shape[1]

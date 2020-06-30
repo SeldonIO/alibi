bf1f4195701296d625b53a6dc224c59d22f09784
Janis Klaise <jk@seldon.io>
2020-06-30 18:28:18 +0100

    Add missing tf1 mark

--- alibi/explainers/tests/test_counterfactual.py
+++ alibi/explainers/tests/test_counterfactual.py
@@ -143,7 +143,7 @@ def test_cf_explainer_iris(cf_iris_explainer):
     if exp.success:
         assert np.abs(pred_class_fn(x_cf) - target_proba) <= tol
 
-
+@pytest.mark.tf1
 @pytest.mark.parametrize('keras_logistic_mnist', ['keras', 'tf'], indirect=True)
 @pytest.mark.parametrize('keras_mnist_cf_explainer', ['other', 'same', 4, 9], indirect=True)
 def test_keras_logistic_mnist_explainer(keras_logistic_mnist, keras_mnist_cf_explainer):

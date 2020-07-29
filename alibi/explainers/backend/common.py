482378248a88b75790700a466114704c58f7a81f
alexcoca <alexcoca23@yahoo.co.uk>
2020-07-29 14:47:57 +0100

    Simplified backend registry
    - Changed backend registry structure to framework -> dependent_class ->
      -> container for whitebox and blackbox backends
    
    - Changed register_backend decorator. It now takes a consumer_class
      argument which specifies the class which is expected to load this
      backend and a framework argument. The predictor_type is now optional
      and defaults to 'whitebox'
    
    - The load_backend function should be used for loading all backend
      modules and is a concrete implementation rather than a dispatcher. To
      avoid setting module names in the calling classes, the function
      assumes that a module with the same name as the calling module exists
      under alibi.explainers.backend.* where * is replaced by the framework
      name.

--- alibi/explainers/backend/common.py
+++ alibi/explainers/backend/common.py
@@ -1,64 +1,36 @@
 # flake8: noqa: F401
-from typing import Optional
+import inspect
+import importlib
+
+from typing_extensions import Literal
 from alibi.explainers.backend import backends_registry
 
 
-def load_backend(explainer_type: str, framework: str, predictor_type: str, method: Optional[str] = None):
+def load_backend(class_name: str,
+                 framework: Literal['pytorch', 'tensorflow'],
+                 predictor_type: Literal['blackbox', 'whitebox']):
     """
-    A dispatch function that delegates the loading of the explainer to specialised functions for each explainer type.
+    Update the backend registry returns the backend for the class specified by `class_name`. _It assumes that the name 
+    of the module where the backend is implemented is the same as the name of the calling context._
 
     Parameters
     ----------
-    explainer_type: {'countefactual', 'cem', 'attribution'}
-        Indicates the type of the explanation algorithm loaded.
+    class_name
+        The name of the class whose backend will be loaded.
     framework: {'pytorch', 'tensorflow'}
-        Indicates which backend should be used.
+        Indicates which backend should be loaded for `class_name`.
     predictor_type: {'blackbox', 'whitebox'}
-        Indicates whether the explainer has access to model parameters.
-    method
-        Indicates the specific implementation for the explainer type (e.g., for counterfactual algorithms 'wachter'
-        means that the backend for the algorithm proposed by `Wachter et al. (2017)`_ (pp. 854) will be loaded).
-        
-        .. _Wachter et al. (2017): 
-           https://jolt.law.harvard.edu/assets/articlePDFs/v31/Counterfactual-Explanations-without-Opening-the-Black-Box-Sandra-Wachter-et-al.pdf
+        'whitebox' indicates that the registered backend has access to the predictor parameters (e.g., it can use the
+        framework autograd functions for differentiation). `blackbox` indicates that the predictor should be treated as
+        a black-box (e.g., numerical differentiation is needed to differentiate the predictor output wrt its input).
     """  # noqa W605
 
-    if explainer_type == 'counterfactual':
-        return load_counterfactual(framework, predictor_type, method)
-    elif explainer_type == 'cem':
-        return load_cem(framework, predictor_type, method)
-    elif explainer_type == 'attribution':
-        return load_attribution(framework, predictor_type, method)
-    else:
-        raise ValueError(f"Unknown explainer type {explainer_type}")
+    # find out where the loader is called from
+    caller_file = inspect.stack()[1].filename
+    # assume a backend module with the same name as the caller exists
+    module_name = caller_file.split("/")[-1][:-3]  # exclude .py
+    backend_module = ".".join([__package__, framework, module_name])
+    # update the backend registry
+    importlib.import_module(backend_module)
 
-
-def load_counterfactual(framework: str, predictor_type: str, method: str):
-    """
-    A loading function that imports the modules where counterfactual algorithms are implemented in order to add the
-    desired framework-specific counterfactual implementation to the explainer registry and then return it.
-
-    Parameters
-    ----------
-    framework, predictor_type, method:
-        See `load_backend` function.
-    """
-
-    # TODO: ALEX: TBD: __init__ alternative
-
-    if framework == 'tensorflow':
-        # import so that the implementation is registered
-        import alibi.explainers.backend.tensorflow.counterfactuals
-    else:
-        import alibi.explainers.backend.pytorch.counterfactual
-        print("backends_registry", backends_registry)
-
-    return backends_registry['counterfactual'][method][framework][predictor_type]
-
-
-def load_cem(framework: str, predictor_type: str, method: str):
-    raise NotImplementedError
-
-
-def load_attribution(framework: str, predictor_type: str, method: str):
-    raise NotImplementedError
+    return backends_registry[framework][class_name][predictor_type]
--- alibi/explainers/backend/registry.py
+++ alibi/explainers/backend/registry.py
@@ -1,129 +1,90 @@
 from collections import defaultdict
-from typing import Optional
+from typing_extensions import Literal
 """
-A module that implements a backend registry along with a decorator to update it. 
+A module that implements a registry containing framework-specific explainer implementaions along with a decorator to 
+update it. 
         
-The registry is updated by decorating the backend classes with the `register_backend` parameterized decorator. The
-`explainer_type` and the `predictor_type` arguments are mandatory. `method` is optional since there might not be 
-multiple methods that implement a particular `explainer_type`. 
+The registry is updated by decorating the backend classes with the `register_backend` parameterized decorator.
     
-The registry should be used to return the backend class using the `alibi.explainers.backend.common.load_backend` 
-function.
+The registry should be used to return the backend class to calling objects using the 
+`alibi.explainers.backend.common.load_backend` function.
 """ # noqa W605
+
+FRAMEWORKS = ['pytorch', 'tensorflow']
 
 
 def framework_factory():
     """
     A factory function that returns a mapping where objects implementing black- and white-box variants of a certain
-    algorithm can be stored.
+    algorithm backend can be stored.
     """
     return {'blackbox': None, 'whitebox': None}
 
 
-def method_dict_factory():
-    """
-    A factory function that returns a mapping where objects implementing black- and white-box variants of a certain
-    algorithm can be stored, for both PyTorch and TensorFlow.
-    """
-    return {'pytorch': framework_factory(), 'tensorflow': framework_factory()}
-
-
-_default_registry = {
-    # we have different methods to search for a cf (e.g., watcher, proto)
-    'counterfactual': {
-        'wachter': {'pytorch': framework_factory(), 'tensorflow': framework_factory()},
-        'proto': {'pytorch': framework_factory(), 'tensorflow': framework_factory()},
-    },
-    # no methods for CEM, but the registry decorator should still be able to register a method if we want to
-    'cem': method_dict_factory(),
-    'attribution': method_dict_factory(),
+backends_registry = {
+    'pytorch': defaultdict(framework_factory),
+    'tensorflow': defaultdict(framework_factory),
 }
-
-backends_registry = defaultdict(method_dict_factory, _default_registry)
 """
-The backend registry is a dictionary
-that is structured as follows::
-    
+The backends registry is a dictionary, structured as follows::
+
     {
-    'explainer_type_1':   
-        {
-        'method_1': {
-                    'pytorch': {'whitebox': None, 'blackbox': None}
-                    'tensorflow': {'whitebox': None, 'blackbox': None}
-                    }
-        'method_2' : {
-                    'pytorch': {'whitebox': None, 'blackbox': None}
-                    'tensorflow': {'whitebox': None, 'blackbox': None}
-                    }
-        }
-    'explainer_type_2':
-        {
-        'pytorch': {'whitebox': None, 'blackbox': None}
-        'tensorflow': {'whitebox': None, 'blackbox': None}
-        }
-    } 
+    'pytorch': {'BackendClass': {'blackbox': None, 'whitebox': None}, ...}
+    'tensorflow': {'BackendClass': {'blackbox': None, 'whitebox': None}, ...}
+    }
     
-    Specifically:
-    
-        - The top level key indicates the explanation algorithm type (e.g., 'counterfactual', 'cem', 'attribution')
-        - An explanation algorithm might have multiple methods (e.g., counterfactual methods would be 'wachter', 'proto', \
-        'offline' defined as the next level keys
-        - If an explainer type doesn't have multiple methods, then a mapping containg the framework names is the next \
-        level. Each key allows storing a 'whitebox' and a 'blackbox' backend of the method/explainer type, in the \
-        framework indicated by the key. 
+    Here `BackendClass` and the framework name are specified by the user using the `register_backend` decorator. Each 
+    backend class can optionally have a predictor type, `whitebox` and `blackbox`. If not specified, the predictor type
+    is assumed to be `whitebox`. See `register_backend` documentation for more details.
 """
 
 
-def register_backend(explainer_type: str, predictor_type: str, method: Optional[str] = None):
+def register_backend(consumer_class: str, predictor_type: Literal['whitebox', 'blackbox'] = 'whitebox'):
     """
-    A parametrized decorator that can be used to register a PyTorch or TensorFlow specific explainer implementations.
-    The decorator is used to access the implementations in various modules in the `alibi.explainers.backend` package.
-    This decorator expects the registered class to have an attribute `framework_name`, set to either 'tensorflow' or
-    'pytorch'.
+    A parametrized decorator that can be used to register a class that contains PyTorch or TensorFlow backend
+    implementations for explainers. The decorator is used to access the implementations in various modules in the
+    `alibi.explainers.backend` package via the `load_backend` function. This decorator expects the registered class to
+    have an attribute `framework_name`, set to either 'tensorflow' or 'pytorch'.
 
     Parameters
     ----------
-    explainer_type: {'countefactual', 'cem', 'attribution'}
-        Indicates the type of the explanation algorithm loaded.
-    predictor_type: {'blackbox', 'whitebox'}
-        Indicates whether the registered backend has access to the predictor parameters.
-    method
-        If passed, the backend registered is a specific implementation of a particular explanation type. For example, a
-        counterfactual explainer admits 'proto' as a method, implementing counterfactual search guided by prototypes.
+    consumer_class
+        This should be the name of the class for which this backend is implemented.
+    predictor_type: {'whitebox', 'blackbox'}
+        'whitebox' indicates that the registered backend has access to the predictor parameters (e.g., it can use the
+        framework autograd functions for differentiation). `blackbox` indicates that the predictor should be treated as
+        a black-box (e.g., numerical differentiation is needed to differentiate the predictor output wrt its input).
+
+    Examples
+    --------
+    The `TFWachterCounterfactualOptimizer` class is a TensorFlow backend for the `_WachterCounterfactual` class. Then,
+    in `alibi.explainers.backend.tensorflow.counterfactual` the registration code would be as follows::
+
+    >>> @register_backend(consumer_class='_WachterCounterfactual', framework='tensorflow')
+    >>> class TFWachterCounterfactualOptimizer(TFCounterfactualOptimizer): ...
+
+    Raises
+    ------
+    ValueError
+        - If the predictor type value is not correct
+        - If the object registred does not have a `framework_name` class attribute
+        - If the `framework_name` is not correctly set
     """
 
-    # TODO: ALEX: TBD: SHOULD WE RAISE THIS ERROR?
-    if explainer_type not in backends_registry:
-        raise ValueError(
-            f"Only backends for explainers of type {backends_registry.keys()} can be registered. Update the registry "
-            f"with a new type if you wish to register a new explainer type!"
-        )
     if predictor_type not in ['blackbox', 'whitebox']:
-        raise ValueError(f"Predictor type must be {predictor_type}")
-
-    backends = backends_registry[explainer_type]
+        raise ValueError(f"Predictor type must be 'blackbox' or 'whitebox' but got {predictor_type}")
 
     def register(obj):
-        if not hasattr(obj, 'framework_name'):
+        if not hasattr(obj, 'framework'):
             raise ValueError(
-                "To register a backend, the class should have an attribute 'framework_name' with value 'tensorflow'"
-                " or 'pytorch'"
+                f"To register a backend, the class should have an attribute 'framework_name' with one of the following "
+                f"values: {FRAMEWORKS}"
             )
-
-        if method:
-            # check if, for the current explainer type, the method specified is already in the default registry
-            # if the method is registred by default, then we just update the field with the object
-            if isinstance(list(backends.keys())[0], str):
-                backends[method][obj.framework_name][predictor_type] = obj
-            # the method specified is not in the default registry. In this case, we need to create a field with the
-            # method name, which maps to a dictionary as returned by `method_dict_factory()`. We update the field
-            # the framework and predictor type, as before.
-            else:
-                backends[method] = method_dict_factory()
-                backends[method][obj.framework_name][predictor_type] = obj
-        # for some algorithms, "method" might not make sense. For those explainer types, the registry contains a key for
-        # each framework as opposed to a key for each method that implements a particular explainer type.
-        else:
-            backends[obj.framework_name][predictor_type] = obj
+        framework = obj.framework
+        if framework not in FRAMEWORKS:
+            raise ValueError(
+                f"Framework must be 'pytorch' or 'tensorflow' but got {framework}"
+            )
+        backends_registry[framework][consumer_class][predictor_type] = obj
         return obj
     return register

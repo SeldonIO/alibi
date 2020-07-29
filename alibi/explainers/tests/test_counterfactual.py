131d113306acc4c8662a5dfbd7ad6be4719085bb
Janis Klaise <jk@seldon.io>
2020-07-29 16:13:55 +0100

    Switch counterfactual MNIST tests to pre-trained models

--- alibi/explainers/tests/test_counterfactual.py
+++ alibi/explainers/tests/test_counterfactual.py
@@ -32,48 +32,11 @@ def cf_iris_explainer(request, logistic_iris):
 
 
 @pytest.fixture
-def keras_logistic_mnist(request):
-    if request.param == 'keras':
-        k = keras
-    elif request.param == 'tf':
-        k = tf.keras
-    else:
-        raise ValueError('Unknown parameter')
-
-    (X_train, y_train), (X_test, y_test) = k.datasets.mnist.load_data()
-    input_dim = 784
-    output_dim = nb_classes = 10
-
-    X = X_train.reshape(60000, input_dim)[:1000]  # only train on 1000 instances
-    X = X.astype('float32')
-    X /= 255
-
-    y = k.utils.to_categorical(y_train[:1000], nb_classes)
-
-    model = k.models.Sequential([
-        k.layers.Dense(output_dim,
-                       input_dim=input_dim,
-                       activation='softmax')
-    ])
-
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-
-    model.fit(X, y, epochs=5)
-
-    yield X, y, model
-    keras.backend.clear_session()
-    tf.keras.backend.clear_session()
-
-
-@pytest.fixture
-def keras_mnist_cf_explainer(request, keras_logistic_mnist):
-    X, y, model = keras_logistic_mnist
-    cf_explainer = CounterFactual(predict_fn=model, shape=(1, 784),
+def keras_mnist_cf_explainer(request, model):
+    cf_explainer = CounterFactual(predict_fn=model, shape=(1, 28, 28, 1),
                                   target_class=request.param, lam_init=1e-1, max_iter=1000,
                                   max_lam_steps=10)
-    yield X, y, model, cf_explainer
+    yield model, cf_explainer
     keras.backend.clear_session()
     tf.keras.backend.clear_session()
 
@@ -104,7 +67,10 @@ def test_define_func(logistic_iris, target_class):
 
 
 @pytest.mark.tf1
-@pytest.mark.parametrize('cf_iris_explainer', ['other', 'same', 0, 1, 2], indirect=True)
+@pytest.mark.parametrize('cf_iris_explainer',
+                         ['other', 'same', 0, 1, 2],
+                         ids='target={}'.format,
+                         indirect=True)
 def test_cf_explainer_iris(disable_tf2, cf_iris_explainer):
     X, y, lr, cf = cf_iris_explainer
     x = X[0].reshape(1, -1)
@@ -142,16 +108,25 @@ def test_cf_explainer_iris(disable_tf2, cf_iris_explainer):
         assert np.abs(pred_class_fn(x_cf) - target_proba) <= tol
 
 
+#TODO: old Keras model missing
 @pytest.mark.tf1
-@pytest.mark.parametrize('keras_logistic_mnist', ['keras', 'tf'], indirect=True)
-@pytest.mark.parametrize('keras_mnist_cf_explainer', ['other', 'same', 4, 9], indirect=True)
-def test_keras_logistic_mnist_explainer(disable_tf2, keras_logistic_mnist, keras_mnist_cf_explainer):
-    X, y, model, cf = keras_mnist_cf_explainer
-    x = X[0].reshape(1, -1)
+@pytest.mark.parametrize('keras_mnist_cf_explainer',
+                         ['other', 'same', 4, 9],
+                         ids='target={}'.format,
+                         indirect=True)
+@pytest.mark.parametrize('model',
+                         ['mnist-logistic-tf2.2.0', 'mnist-logistic-tf1.15.0.h5'],
+                         ids='model={}'.format,
+                         indirect=True)
+def test_keras_mnist_explainer(disable_tf2, keras_mnist_cf_explainer, get_mnist_dataset):
+    model, cf = keras_mnist_cf_explainer
+    X = get_mnist_dataset['X_train']
+
+    x = X[0:1]
     probas = cf.predict_fn(x)
     pred_class = probas.argmax()
 
-    assert cf.data_shape == (1, 784)
+    assert cf.data_shape == (1, 28, 28, 1)
 
     # test explanation
     exp = cf.explain(x)

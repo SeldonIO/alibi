9bd29eea2005715e23e26e27d53afe789c4d70dc
Janis Klaise <jk@seldon.io>
2021-03-15 17:02:27 +0000

    Deprecate fetch_imagenet and add load_cats with some example data

--- alibi/tests/test_datasets.py
+++ alibi/tests/test_datasets.py
@@ -1,6 +1,6 @@
 import pytest
 from requests import RequestException
-from alibi.datasets import fetch_adult, fetch_imagenet, fetch_movie_sentiment
+from alibi.datasets import fetch_adult, fetch_imagenet, fetch_movie_sentiment, load_cats
 
 # TODO use mocking instead of relying on external services
 
@@ -71,3 +71,18 @@ def test_movie_sentiment(return_X_y):
 
     assert len(X) == len(y)
     assert len(set(y)) == MOVIE_CLASSES
+
+
+@pytest.mark.parametrize('return_X_y', [True, False])
+@pytest.mark.parametrize('target_size', [(299, 299)])
+def test_cats(return_X_y, target_size):
+    data = load_cats(target_size=target_size, return_X_y=return_X_y)
+    if return_X_y:
+        assert len(data) == 2
+        X, y = data
+    else:
+        assert len(data) == 3
+        X = data.data
+        y = data.target
+    assert len(X) == len(y)
+    assert X.shape[1:] == target_size + (3,)  # 3 channels

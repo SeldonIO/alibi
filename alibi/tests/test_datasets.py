9550a4a7976a63ed907733b46cf304ab9bb0e91a
Janis Klaise <jk@seldon.io>
2021-03-15 17:08:35 +0000

    Remove Imagenet test

--- alibi/tests/test_datasets.py
+++ alibi/tests/test_datasets.py
@@ -29,29 +29,6 @@ def test_adult(return_X_y):
     assert len(set(y)) == ADULT_CLASSES
 
 
-@pytest.mark.parametrize('nb_images', [3])
-@pytest.mark.parametrize('category', [
-    'Persian cat', 'volcano', 'strawberry', 'Siamese cat', 'Siamese', 'Siamese cat, Siamese'])
-@pytest.mark.parametrize('return_X_y', [True, False])
-def test_imagenet(nb_images, category, return_X_y):
-    try:
-        data = fetch_imagenet(category=category, nb_images=nb_images, target_size=(299, 299), return_X_y=return_X_y)
-    except RequestException:
-        pytest.skip('Imagenet API down')
-
-    if return_X_y:
-        X, y = data
-    else:
-        X = data.data
-        y = data.target
-
-    assert X.shape == (nb_images, 299, 299, 3)  # 3 color channels
-    assert X.max() <= 255  # RGB limits
-    assert X.min() >= 0
-
-    assert len(y) == nb_images
-
-
 MOVIE_CLASSES = 2
 
 

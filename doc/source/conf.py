ecad082d860045bbee6d8b75de96143b1052779b
Janis Klaise <jk@seldon.io>
2021-04-29 14:56:20 +0100

    Add dill to sphinx mocks

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,7 +71,7 @@ apidoc_extra_args = ['-d 6']
 
 # mock imports
 autodoc_mock_imports = ['sklearn', 'skimage', 'requests',
-                        'cv2', 'keras', 'seaborn', 'PIL', 'tensorflow', 'spacy', 'catboost']
+                        'cv2', 'keras', 'seaborn', 'PIL', 'tensorflow', 'spacy', 'catboost', 'dill']
 
 # Napoleon settings
 napoleon_google_docstring = True

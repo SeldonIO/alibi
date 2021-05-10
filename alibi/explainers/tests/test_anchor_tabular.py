ce30b8859985257b786c66c77474244fe318e3a3
Janis Klaise <jk@seldon.io>
2021-05-10 09:41:49 +0100

    Lint with flake8

--- alibi/explainers/tests/test_anchor_tabular.py
+++ alibi/explainers/tests/test_anchor_tabular.py
@@ -90,6 +90,7 @@ def test_explainer(n_explainer_runs, at_defaults, rf_classifier, explainer, test
     assert sampler.instance_label == instance_label
     assert sampler.n_covered_ex == n_covered_ex
 
+
 @pytest.mark.skip(reason='Not testing as performance of distributed anchors (within an instance) not clear.'
                          'Also, these tests fail intermittently with precision treshold not achieved.')
 @pytest.mark.parametrize('ncpu', [2], ids='ncpu={}'.format)

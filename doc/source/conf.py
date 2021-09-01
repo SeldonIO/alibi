85d7daf6fc04b2eaf2edac3e2db5e2cd7a1f4488
Janis Klaise <jk@seldon.io>
2021-09-01 15:09:16 +0100

    Mock torch and matplotlib in sphinx build

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -72,7 +72,7 @@ apidoc_extra_args = ['-d 6']
 # mock imports
 autodoc_mock_imports = ['sklearn', 'skimage', 'requests',
                         'cv2', 'keras', 'seaborn', 'PIL', 'tensorflow', 'spacy', 
-                        'catboost', 'dill', 'transformers']
+                        'catboost', 'dill', 'transformers', 'torch', 'matplotlib']
 
 # Napoleon settings
 napoleon_google_docstring = True

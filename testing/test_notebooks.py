078c32b41cec8a7c09a5de648b0e7bc519a1b873
Janis Klaise <jk@seldon.io>
2021-09-23 13:27:53 +0100

    Clean up test script and fix issue with passing a Path to jupytext

--- testing/test_notebooks.py
+++ testing/test_notebooks.py
@@ -3,7 +3,6 @@ This script is an example of using `jupytext` to execute notebooks for testing i
 plugin. This approach may be more flexible if our requirements change in the future.
 """
 
-import argparse
 import glob
 from pathlib import Path
 import pytest
@@ -17,7 +16,6 @@ from jupytext.cli import jupytext
 # top-level path.
 NOTEBOOK_DIR = 'examples'
 ALL_NOTEBOOKS = {Path(x).name for x in glob.glob('examples/*.ipynb')}
-# notebooks = glob.glob('examples/*.ipynb')
 
 # The following set includes notebooks which are not to be executed during notebook tests.
 # These are typically those that would take too long to run in a CI environment or impractical
@@ -29,17 +27,8 @@ EXCLUDE_NOTEBOOKS = {
 EXECUTE_NOTEBOOKS = ALL_NOTEBOOKS - EXCLUDE_NOTEBOOKS
 
 
-@pytest.mark.timeout(300)
+@pytest.mark.timeout(600)
 @pytest.mark.parametrize("notebook", EXECUTE_NOTEBOOKS)
 def test_notebook_execution(notebook):
     notebook = Path(NOTEBOOK_DIR, notebook)
-    jupytext(args=[notebook, "--execute"])
-
-
-if __name__ == '__main__':
-    # When run as a script, execute the notebook passed on the command line
-    parser = argparse.ArgumentParser()
-    parser.add_argument('notebook', type=str)
-    args = parser.parse_args()
-
-    test_notebook_execution(args.notebook)
+    jupytext(args=[str(notebook), "--execute"])

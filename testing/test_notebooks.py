18385a1d26302c9c9c83466594c310f8011a1162
Janis Klaise <jk@seldon.io>
2021-09-27 12:18:16 +0100

    Notebook testing v2 (#484)
    * Use pytest for testing and test collection
    
    * Move comment to avoid syntax error
    
    * Replace brackets with parens
    
    * Debug with tmate
    
    * Set changed notebooks as an environment variable
    
    * Properly pass variable
    
    * Enable jupytext and pytest-timeout
    
    * Clean up test script and fix issue with passing a Path to jupytext
    
    * Remove nbmake and update pytest commands
    
    * Remove test notebooks
    
    * Add missing test dependencies
    
    * Update nbconvert version for Python 3.6
    
    * Exclude long-running notebooks
    
    * Exclude more long-running notebooks
    
    * Update readme for running regular tests
    
    * Enable weekly cron job

--- testing/test_notebooks.py
+++ testing/test_notebooks.py
@@ -3,25 +3,41 @@ This script is an example of using `jupytext` to execute notebooks for testing i
 plugin. This approach may be more flexible if our requirements change in the future.
 """
 
-import argparse
 import glob
+from pathlib import Path
 import pytest
 from jupytext.cli import jupytext
 
-# list of all example notebooks
-notebooks = glob.glob('examples/*.ipynb')
+# Set of all example notebooks
+# NOTE: we specifically get only the name of the notebook not the full path as we want to
+# use these as variables on the command line for `pytest` for the workflow executing only
+# changed notebooks. `pytest` does not allow `/` as part of the test name for the -k argument.
+# This also means that the approach is limited to all notebooks being in the `NOTEBOOK_DIR`
+# top-level path.
+NOTEBOOK_DIR = 'examples'
+ALL_NOTEBOOKS = {Path(x).name for x in glob.glob('examples/*.ipynb')}
+
+# The following set includes notebooks which are not to be executed during notebook tests.
+# These are typically those that would take too long to run in a CI environment or impractical
+# due to other dependencies (e.g. downloading large datasets
+EXCLUDE_NOTEBOOKS = {
+    # the following are all long-running
+    'anchor_text_movie.ipynb',  # `autoregressive` filling example
+    'cem_mnist.ipynb',  # black-box example
+    'cfrl_mnist.ipynb',
+    'cfrl_adult.ipynb',
+    'cfproto_mnist.ipynb',  # black-box example
+    'distributed_kernel_shap_adult_lr.ipynb',  # sequential explainer cell
+    'interventional_tree_shap_adult_xgb.ipynb',  # comparison with KernelShap
+    'kernel_shap_adult_lr.ipynb',  # slow to explain 128 instances
+    'xgboost_model_fitting_adult.ipynb',  # very expensive hyperparameter tuning
+    'integrated_gradients_transformers.ipynb',  # forward pass through BERT to get embeddings is very slow
+}
+EXECUTE_NOTEBOOKS = ALL_NOTEBOOKS - EXCLUDE_NOTEBOOKS
 
 
-@pytest.mark.timeout(300)
-@pytest.mark.parametrize("notebook", notebooks)
+@pytest.mark.timeout(600)
+@pytest.mark.parametrize("notebook", EXECUTE_NOTEBOOKS)
 def test_notebook_execution(notebook):
-    jupytext(args=[notebook, "--execute"])
-
-
-if __name__ == '__main__':
-    # When run as a script, execute the notebook passed on the command line
-    parser = argparse.ArgumentParser()
-    parser.add_argument('notebook', type=str)
-    args = parser.parse_args()
-
-    test_notebook_execution(args.notebook)
+    notebook = Path(NOTEBOOK_DIR, notebook)
+    jupytext(args=[str(notebook), "--execute"])

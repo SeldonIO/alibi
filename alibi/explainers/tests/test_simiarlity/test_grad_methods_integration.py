fabb525fdb170ecc62e29fb78be617ee36d859c2
Alex Athorne <alex.athorne@seldon.io>
2022-02-08 17:39:53 +0000

    Add tensorflow integration tests

--- alibi/explainers/tests/test_simiarlity/test_grad_methods_integration.py
+++ alibi/explainers/tests/test_simiarlity/test_grad_methods_integration.py
@@ -1,16 +1,34 @@
-# import numpy as np
-# import pytest
-# from alibi.explainers.similarity.grad import SimilarityExplainer
-# from alibi.explainers.tests.test_simiarlity.conftest import get_flattened_model_parameters
+import numpy as np
+from alibi.explainers.similarity.grad import SimilarityExplainer
+import torch.nn as nn
 
 
-def test_same_class_grad_dot():
-    """
-    Test that the grad-dot similarity methods highest scoring training point is the same class as test point.
-    """
+def test_same_class_grad_dot_torch():
+    """"""
+    np.random.seed(seed=0)
+    ds = np.random.normal(size=(100, 2)).astype('float32')
+    ds = ds/np.linalg.norm(ds, axis=1)[:, None]
+    model = nn.Linear(2, 1, bias=False)
+    loss = lambda x, y: x
+    explainer = SimilarityExplainer(model, task='regression', loss_fn=loss, sim_fn='grad_dot', backend='torch')
+    explainer = explainer.fit(ds, ds)
+    target_fn = lambda x: x
+    explanation = explainer.explain(ds[0], y=target_fn)
+    close_score = np.dot(ds[0], explanation['x_train'][1])
+    np.testing.assert_almost_equal(close_score, 1, decimal=2)
+    far_score = np.dot(ds[0], explanation['x_train'][-1])
+    np.testing.assert_almost_equal(far_score, -1, decimal=2)
 
 
 def test_same_class_grad_cos():
-    """
-    Test that the grad-cos similarity methods highest scoring training point is the same class as test point.
-    """
+    """"""
+    np.random.seed(seed=0)
+    ds = np.array([[1, 0], [0.9, 0.1], [0.5 * 100, 0.5 * 100]]).astype('float32')
+    model = nn.Linear(2, 1, bias=False)
+    loss = lambda x, y: x
+    explainer = SimilarityExplainer(model, task='regression', loss_fn=loss, sim_fn='grad_cos', backend='torch')
+    explainer = explainer.fit(ds, ds)
+    target_fn = lambda x: x
+    explanation = explainer.explain(ds[0], y=target_fn)
+    assert (explanation['x_train'][1] == ds[1]).all()
+    assert (explanation['x_train'][-1] == ds[-1]).all()

cb09fbc26fb226bd7e746beb145166f92b2842fa
Alex Athorne <alex.athorne@seldon.io>
2022-03-15 12:03:25 +0000

    Remove print statment in setup.py

--- setup.py
+++ setup.py
@@ -16,8 +16,6 @@ def get_extra_requires(path, add_all=True):
     """
     import re
     from collections import defaultdict
-    import os
-    print(os.getcwd())
 
     with open(path) as fp:
         extra_deps = defaultdict(set)
@@ -38,35 +36,36 @@ def get_extra_requires(path, add_all=True):
     return extra_deps
 
 
-setup(name='alibi',
-      author='Seldon Technologies Ltd.',
-      author_email='hello@seldon.io',
-      version=__version__,  # type: ignore # noqa F821
-      description='Algorithms for monitoring and explaining machine learning models',
-      long_description=readme(),
-      long_description_content_type='text/markdown',
-      url='https://github.com/SeldonIO/alibi',
-      license='Apache 2.0',
-      packages=find_packages(),
-      include_package_data=True,
-      python_requires='>=3.7',
-      # lower bounds based on Debian Stable versions where available
-      install_requires=[
-          'numpy>=1.16.2, <2.0.0',
-          'pandas>=0.23.3, <2.0.0',
-          'scikit-learn>=0.20.2, <1.1.0',
-          'spacy[lookups]>=2.0.0, <4.0.0',
-          'scikit-image>=0.14.2, !=0.17.1, <0.19',  # https://github.com/SeldonIO/alibi/issues/215
-          'requests>=2.21.0, <3.0.0',
-          'Pillow>=5.4.1, <9.0',
-          'attrs>=19.2.0, <22.0.0',
-          'scipy>=1.1.0, <2.0.0',
-          'matplotlib>=3.0.0, <4.0.0',
-          'typing-extensions>=3.7.2; python_version < "3.8"',  # https://github.com/SeldonIO/alibi/pull/248
-          'dill>=0.3.0, <0.4.0',
-          'transformers>=4.7.0, <5.0.0',
-          'tqdm>=4.28.1, <5.0.0'
-      ],
-      extras_require=get_extra_requires('./extra-requirements.txt'),
-      test_suite='tests',
-      zip_safe=False)
+if __name__ == '__main__':
+    setup(name='alibi',
+          author='Seldon Technologies Ltd.',
+          author_email='hello@seldon.io',
+          version=__version__,  # type: ignore # noqa F821
+          description='Algorithms for monitoring and explaining machine learning models',
+          long_description=readme(),
+          long_description_content_type='text/markdown',
+          url='https://github.com/SeldonIO/alibi',
+          license='Apache 2.0',
+          packages=find_packages(),
+          include_package_data=True,
+          python_requires='>=3.7',
+          # lower bounds based on Debian Stable versions where available
+          install_requires=[
+              'numpy>=1.16.2, <2.0.0',
+              'pandas>=0.23.3, <2.0.0',
+              'scikit-learn>=0.20.2, <1.1.0',
+              'spacy[lookups]>=2.0.0, <4.0.0',
+              'scikit-image>=0.14.2, !=0.17.1, <0.19',  # https://github.com/SeldonIO/alibi/issues/215
+              'requests>=2.21.0, <3.0.0',
+              'Pillow>=5.4.1, <9.0',
+              'attrs>=19.2.0, <22.0.0',
+              'scipy>=1.1.0, <2.0.0',
+              'matplotlib>=3.0.0, <4.0.0',
+              'typing-extensions>=3.7.2; python_version < "3.8"',  # https://github.com/SeldonIO/alibi/pull/248
+              'dill>=0.3.0, <0.4.0',
+              'transformers>=4.7.0, <5.0.0',
+              'tqdm>=4.28.1, <5.0.0'
+          ],
+          extras_require=get_extra_requires('./extra-requirements.txt'),
+          test_suite='tests',
+          zip_safe=False)

5fa9ed43f8bb569b9fdba12975acd557a609c3b5
Alex Athorne <alex.athorne@seldon.io>
2022-04-04 13:58:55 +0100

    Fix failing save test

--- alibi/tests/test_saving.py
+++ alibi/tests/test_saving.py
@@ -464,7 +464,7 @@ def test_save_SimilartyExplainer(similarity_explainer, ffn_classifier, iris_data
         exp1 = similarity_explainer1.explain(X)
         assert exp0.meta == exp1.meta
         assert (exp0.data['scores'] == exp1.data['scores']).all()
-        assert (exp0.data['X_train'] == exp1.data['X_train']).all()
-        assert (exp0.data['Y_train'] == exp1.data['Y_train']).all()
+        assert (exp0.data['ordered_X_train'] == exp1.data['ordered_X_train']).all()
+        assert (exp0.data['ordered_Y_train'] == exp1.data['ordered_Y_train']).all()
         assert (exp0.data['most_similar'] == exp1.data['most_similar']).all()
         assert (exp0.data['least_similar'] == exp1.data['least_similar']).all()

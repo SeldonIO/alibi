ab732a3aa5099f4915762eb2535328fe44de17c1
mauicv <alex.athorne@seldon.io>
2022-04-04 13:28:00 +0100

    Revert "Add bounds to click to prevent CI error (#617)" (#623)
    This reverts commit c67dcac7790d609cc5a497a3010c6238f6fc19e9.

--- setup.py
+++ setup.py
@@ -46,8 +46,7 @@ setup(name='alibi',
           'typing-extensions>=3.7.4.3',
           'dill>=0.3.0, <0.4.0',
           'transformers>=4.7.0, <5.0.0',
-          'tqdm>=4.28.1, <5.0.0',
-          'click<8.1.0' # remove once https://github.com/explosion/spaCy/pull/10573 is released
+          'tqdm>=4.28.1, <5.0.0'
       ],
       extras_require=extras_require,
       test_suite='tests',

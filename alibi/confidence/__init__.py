667dce7838ef8efc441887c235040521df0bd4f4
Alex Athorne <alex.athorne@seldon.io>
2022-04-05 15:54:42 +0100

    Rename _infer_feature_range to be public

--- alibi/confidence/__init__.py
+++ alibi/confidence/__init__.py
@@ -3,9 +3,9 @@ The 'alibi.confidence' module includes trust scores.
 """
 
 from .trustscore import TrustScore
-from .model_linearity import LinearityMeasure, linearity_measure, _infer_feature_range
+from .model_linearity import LinearityMeasure, linearity_measure, infer_feature_range
 
 __all__ = ["linearity_measure",
            "LinearityMeasure",
            "TrustScore",
-           "_infer_feature_range"]
+           "infer_feature_range"]

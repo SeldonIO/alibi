7f4447dd9244e05d76d07d487b525c26989ff28d
Alex Athorne <alex.athorne@seldon.io>
2022-04-21 17:25:05 +0100

    Refactor confidence imports

--- alibi/confidence/__init__.py
+++ alibi/confidence/__init__.py
@@ -3,9 +3,8 @@ The 'alibi.confidence' module includes trust scores.
 """
 
 from .trustscore import TrustScore
-from .model_linearity import LinearityMeasure, linearity_measure, infer_feature_range
+from .model_linearity import LinearityMeasure, linearity_measure
 
 __all__ = ["linearity_measure",
            "LinearityMeasure",
-           "TrustScore",
-           "infer_feature_range"]
+           "TrustScore"]

fix types multidim scaling

alibi/explainers/cfproto.py
@@ -38,7 +38,7 @@ class CounterfactualProto(Explainer, FitMixin):
                  shape: tuple,
                  kappa: float = 0.,
                  beta: float = .1,
-                 feature_range: tuple = (-1e10, 1e10),
+                 feature_range: Tuple[Union[float, np.ndarray], Union[float, np.ndarray]] = (-1e10, 1e10),
                  gamma: float = 0.,
                  ae_model: Optional[tf.keras.Model] = None,
                  enc_model: Optional[tf.keras.Model] = None,
@@ -178,7 +178,11 @@ class CounterfactualProto(Explainer, FitMixin):
         self.max_iterations = max_iterations
         self.c_init = c_init
         self.c_steps = c_steps
-        self.feature_range = feature_range
+        self.feature_range = list(feature_range)
+        for _ in range(2):
+            if isinstance(feature_range[_], float):
+                self.feature_range[_] = (np.ones(shape[1:]) * feature_range[_])[None, :]
+        self.feature_range = tuple(self.feature_range)
         self.update_num_grad = update_num_grad
         self.eps = eps
         self.clip = clip

alibi/utils/distance.py
@@ -162,7 +162,7 @@ def abdm(X: np.ndarray,
 
 
 def multidim_scaling(d_pair: dict,
-                     feature_range: tuple,
+                     feature_range: Tuple[np.ndarray, np.ndarray],
                      n_components: int = 2,
                      use_metric: bool = True,
                      standardize_cat_vars: bool = True,
@@ -178,8 +178,8 @@ def multidim_scaling(d_pair: dict,
         Dict with as keys the column index of the categorical variables and as values
         a pairwise distance matrix for the categories of the variable.
     feature_range
-        Tuple with `min` and `max` ranges to allow for perturbed instances. `Min` and `max` ranges can be `float` or
-        `numpy` arrays with dimension (`1 x nb of features`) for feature-wise ranges.
+        Tuple with `min` and `max` ranges to allow for perturbed instances. `Min` and `max` ranges are
+        `numpy` arrays with dimension (`1 x nb of features`).
     n_components
         Number of dimensions in which to immerse the dissimilarities.
     use_metric
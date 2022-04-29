bee8c59f35d9a89071f38edc9e0fb436b5d56e84
RobertSamoilescu <robert.samoilescu@gmail.com>
2022-04-29 18:55:20 +0300

    exposed visualize_image_prototype to alibi.prototypes

--- alibi/prototypes/__init__.py
+++ alibi/prototypes/__init__.py
@@ -3,5 +3,7 @@ The 'alibi.prototypes' modules includes prototypes and criticism selection metho
 """
 
 from .protoselect import ProtoSelect
+from .protoselect import visualize_image_prototypes
 
-__all__ = ["ProtoSelect"]
+__all__ = ['ProtoSelect',
+           'visualize_image_prototypes']

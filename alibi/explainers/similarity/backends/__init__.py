dbb3dc7b933a566f6a4d51cd51e8f10e0a7141a8
mauicv <alex.athorne@seldon.io>
2022-05-12 17:14:27 +0100

    Add backend verification for GradientSimilarity (#656)

--- alibi/explainers/similarity/backends/__init__.py
+++ alibi/explainers/similarity/backends/__init__.py
@@ -12,7 +12,7 @@ if has_tensorflow:
 
 
 def _select_backend(backend: Framework = Framework.TENSORFLOW) \
-        -> Union[Type[_TensorFlowBackend], Type[_PytorchBackend]]:
+        -> Union[Type['_TensorFlowBackend'], Type['_PytorchBackend']]:
     """
     Selects the backend according to the `backend` flag.
 
@@ -21,4 +21,14 @@ def _select_backend(backend: Framework = Framework.TENSORFLOW) \
     backend
         Deep learning backend.
     """
+    # Check if pytorch/tensorflow backend supported.
+    if (backend == Framework.PYTORCH and not has_pytorch) or \
+            (backend == Framework.TENSORFLOW and not has_tensorflow):
+        raise ImportError(f'{backend} not installed. Cannot initialize and run the GradientExplainer'
+                          f' with {backend} backend.')
+
+    # Allow only pytorch and tensorflow.
+    elif backend not in [Framework.PYTORCH, Framework.TENSORFLOW]:
+        raise NotImplementedError(f'{backend} not implemented. Use `tensorflow` or `pytorch` instead.')
+
     return _TensorFlowBackend if backend == Framework.TENSORFLOW else _PytorchBackend

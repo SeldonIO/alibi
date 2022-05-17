f362fccde005e0daab4311b0a5bb69b3ec309947
Janis Klaise <jk@seldon.io>
2022-05-17 14:20:07 +0100

    Fix typo; exclude only from Windows build

--- testing/test_notebooks.py
+++ testing/test_notebooks.py
@@ -34,10 +34,10 @@ EXCLUDE_NOTEBOOKS = {
     'xgboost_model_fitting_adult.ipynb',  # very expensive hyperparameter tuning
     'integrated_gradients_transformers.ipynb',  # forward pass through BERT to get embeddings is very slow
     'similarity_explanations_imagenet.ipynb',  # forward pass through ResNet too slow
-    'similarity_explanations_20ng.ipymb', # times out, likely due to the EmbeddingModel being slow
 }
 if platform.system() == 'Windows':
    EXCLUDE_NOTEBOOKS.add('protoselect_adult_cifar10.ipynb')  # Exclude <your notebook> on Windows due to the use of wget
+   EXCLUDE_NOTEBOOKS.add('similarity_explanations_20ng.ipynb')  # times out, likely due to the EmbeddingModel being slow
 EXECUTE_NOTEBOOKS = ALL_NOTEBOOKS - EXCLUDE_NOTEBOOKS
 
 @pytest.mark.timeout(600)

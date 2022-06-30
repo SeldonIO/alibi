4e73c6f00a60eece3e91d804f7eb3956418c06b8
Robert Samoilescu <robert.samoilescu@gmail.com>
2022-06-30 15:21:03 +0100

    Updated method params docs and sync with docs

--- alibi/explainers/anchors/anchor_image.py
+++ alibi/explainers/anchors/anchor_image.py
@@ -473,10 +473,11 @@ class AnchorImage(Explainer):
         threshold
             Minimum anchor precision threshold. The algorithm tries to find an anchor that maximizes the coverage
             under precision constraint. The precision constraint is formally defined as
-            :math:`P(prec(A) \ge t) \ge 1 - \delta`, where :math:`A` is an anchor, :math:`t` is the `threshold`
-            parameter, :math:`\delta` is the `delta` parameter, and :math:`prec(\cdot)` denotes the precision
+            :math:`P(prec(A) \\ge t) \\ge 1 - \\delta`, where :math:`A` is an anchor, :math:`t` is the `threshold`
+            parameter, :math:`\\delta` is the `delta` parameter, and :math:`prec(\\cdot)` denotes the precision
             of an anchor. In other words, we are seeking for an anchor having its precision greater or equal than
-            the given `threshold` with a confidence of `(1 - delta)`. Note that there are cases in which the precision
+            the given `threshold` with a confidence of `(1 - delta)`. A higher value gives more confidence in the
+            anchor, but also leads to more computation time. Note that there are cases in which the precision
             constraint cannot be satisfied due to the quantile-based discretisation of the numerical features.
             If that is the case, the best (i.e. highest coverage) non-eligible anchor is returned.
         delta
@@ -487,20 +488,23 @@ class AnchorImage(Explainer):
             algorithm tries to find the potentially best (i.e. highest precision) `beam_size` candidate anchors from a
             list of anchors created by including a new predicate in the candidate anchors form the previous iteration.
             Formally, when the `beam_size=1`, the multi-armed bandit algorithm seeks to find an anchor :math:`A` such
-            that :math:`P(prec(A) \ge prec(A^\star) - \\tau) \ge 1 - \delta`, where :math:`A^\star` is the anchor
-            with the highest true precision, :math:`\\tau` is the `tau` parameter, :math:`\delta` is the `delta`
-            parameter, and :math:`prec(\cdot)` denotes the precision of an anchor. In other words, in each iteration,
+            that :math:`P(prec(A) \\ge prec(A^\\star) - \\tau) \\ge 1 - \\delta`, where :math:`A^\\star` is the anchor
+            with the highest true precision, :math:`\\tau` is the `tau` parameter, :math:`\\delta` is the `delta`
+            parameter, and :math:`prec(\\cdot)` denotes the precision of an anchor. In other words, in each iteration,
             the algorithm returns with a probability of at least `1 - delta` an anchor :math:`A` with a precision lower
-            than the precision of the highest precision anchor in the current iterration, :math:`A^\star`,
-            with a maximum tolerance of `tau`.
+            than the precision of the highest precision anchor in the current iteration, :math:`A^\\star`,
+            with a maximum tolerance of `tau`. A bigger value for `tau` means faster convergence but also looser
+            anchor conditions.
         batch_size
             Batch size used for sampling. The Anchor algorithm will query the black-box model in batches of size
-            `batch_size`.
+            `batch_size`. A larger `batch_size` gives more confidence in the anchor, again at the expense of
+            computation time since it involves more model prediction calls.
         coverage_samples
             Number of samples used to estimate coverage from during result search.
         beam_size
             The number of anchors extended (i.e. candidate anchors returned by the multi-armed bandit)
-            at each step of new anchors construction.
+            at each step of new anchors construction. A bigger beam width can lead to a better overall anchor at the
+            expense of more computation time.
         stop_on_first
             If ``True``, the beam search algorithm will return the first anchor that has satisfies the
             probability constraint.

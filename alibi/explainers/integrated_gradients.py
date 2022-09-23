Fix/integrated gradients int check 2 (#781)
* Fix mistake in checking if target array is integers in IntegratedGradients

* Remove unused import

alibi/explainers/integrated_gradients.py
@@ -1,5 +1,3 @@
-import numbers
-
 import copy
 import logging
 import string
@@ -586,7 +584,7 @@ def _check_target(output_shape: Tuple,
 
     if target is not None:
 
-        if not isinstance(target.dtype, numbers.Integral):
+        if not np.issubdtype(target.dtype, np.integer):
             raise ValueError("Targets must be integers")
 
         if target.shape[0] != nb_samples:
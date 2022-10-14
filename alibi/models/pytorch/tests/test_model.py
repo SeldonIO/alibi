9193c83603dd60ec7a6df70ba28df26a82077171
RobertSamoilescu <robert.samoilescu@gmail.com>
2022-10-14 14:46:37 +0100

    isort, included docs, consisten use of quotes.

--- alibi/models/pytorch/tests/test_model.py
+++ alibi/models/pytorch/tests/test_model.py
@@ -1,34 +1,83 @@
-import re
 import os
+import re
+import tempfile
+from typing import List
+
 import numpy as np
 import pytest
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import TensorDataset, DataLoader
-from alibi.models.pytorch.model import Model
 from alibi.models.pytorch.metrics import AccuracyMetric, LossContainer
-from typing import List
-import tempfile
+from alibi.models.pytorch.model import Model
+from torch.utils.data import DataLoader, TensorDataset
 
 
 class UnimodalModel(Model):
+    """ Simple uni-modal output model. """
+
     def __init__(self, input_dim: int, output_dim: int):
+        """
+        Initializer.
+
+        Parameters
+        ----------
+        input_dim
+            Input dimension.
+        output_dim
+            Output dimension.
+        """
         super().__init__()
         self.fc1 = nn.Linear(input_dim, output_dim)
         self.to(self.device)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
+        """
+        Forward pass
+
+        Parameters
+        ----------
+        x
+            Input tensor.
+
+        Returns
+        -------
+        Prediction tensor.
+        """
         return self.fc1(x)
 
 
 class MultimodalModel(Model):
+    """ Simple multi-modal output model. """
+
     def __init__(self, input_dim: int, output_dims: List[int]):
+        """
+        Initializer.
+
+        Parameters
+        ----------
+        input_dim
+            Input dimension.
+        output_dims
+            List of output dimensions for each modality.
+        """
         super().__init__()
         self.fcs = nn.ModuleList([nn.Linear(input_dim, dim) for dim in output_dims])
         self.to(self.device)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
+        """
+        Forward pass.
+
+        Parameters
+        ----------
+        x
+            Input tensor.
+
+        Returns
+        -------
+        List of prediction tensors.
+        """
         return [fc(x) for fc in self.fcs]
 
 
@@ -79,8 +128,9 @@ def test_validate_prediction_labels1(multimodal_model):
     """ Test if an error is raised when multiple loss function were compiled but the model outputs is a tensor
     instead of a list of tensors. """
     multimodal_model.compile(optimizer=optim.Adam(multimodal_model.parameters()),
-                             loss=[nn.MSELoss(reduction="mean"), nn.BCELoss(reduction="mean")],
+                             loss=[nn.MSELoss(reduction='mean'), nn.CrossEntropyLoss(reduction='mean')],
                              loss_weights=[1, 1])
+
     y_pred = torch.randn(5, 5)
     y_true = [torch.randn(5, 5), torch.randn(5, 3)]
     with pytest.raises(ValueError) as err:
@@ -93,7 +143,7 @@ def test_validate_prediction_labels2(multimodal_model):
     """ Test if an error is raised when multiple loss functions were compiled but the target is a tensor
     instead of a list of tensors. """
     multimodal_model.compile(optimizer=optim.Adam(multimodal_model.parameters()),
-                             loss=[nn.MSELoss(reduction="mean"), nn.BCELoss(reduction="mean")],
+                             loss=[nn.MSELoss(reduction='mean'), nn.BCELoss(reduction='mean')],
                              loss_weights=[1, 1])
     y_pred = [torch.randn(5, 5), torch.randn(5, 3)]
     y_true = torch.randn(5, 5)
@@ -114,7 +164,7 @@ def test_validate_prediction_labels3(multimodal_model):
     y_true = [torch.randn(5, 5)]
     with pytest.raises(ValueError) as err:
         multimodal_model.validate_prediction_labels(y_pred=y_pred, y_true=y_true)
-    assert re.search("Number of predictions differs from the number of labels.", err.value.args[0])
+    assert re.search('Number of predictions differs from the number of labels.', err.value.args[0])
 
 
 @pytest.mark.parametrize('unimodal_model', [{'input_dim': 10, 'output_dim': 10}], indirect=True)
@@ -122,19 +172,19 @@ def test_validate_prediction_labels4(unimodal_model):
     """ Test if an error is raised when a single loss function is complied but the prediction
     is a list of tensors. """
     unimodal_model.compile(optimizer=optim.Adam(unimodal_model.parameters()),
-                           loss=nn.MSELoss(reduction="mean"))
+                           loss=nn.MSELoss(reduction='mean'))
     y_pred = [torch.randn(5, 5), torch.randn(5, 5)]
     y_true = torch.randn(5, 5)
     with pytest.raises(ValueError) as err:
         unimodal_model.validate_prediction_labels(y_pred=y_pred, y_true=y_true)
-    assert re.search("The prediction is a list and should be a tensor since only one loss has been passed",
+    assert re.search('The prediction is a list and should be a tensor since only one loss has been passed',
                      err.value.args[0])
 
 
 @pytest.mark.parametrize('unimodal_model', [{'input_dim': 10, 'output_dim': 10}], indirect=True)
 def test_compute_loss_unimodal(unimodal_model):
     """ Test if the loss computation for a single loss function matches the expectation. """
-    loss = nn.MSELoss(reduction="mean")
+    loss = nn.MSELoss(reduction='mean')
     unimodal_model.compile(optimizer=optim.Adam(unimodal_model.parameters()), loss=nn.MSELoss(loss))
 
     y_true = torch.randn(10, 10)
@@ -148,8 +198,8 @@ def test_compute_loss_unimodal(unimodal_model):
 @pytest.mark.parametrize('multimodal_model', [{'input_dim': 10, 'output_dims': [5, 3]}], indirect=True)
 def test_compute_loss_multimodal(multimodal_model):
     """ Test if the loss computation for multiple loss functions matches the expectation. """
-    loss1 = nn.MSELoss(reduction="mean")
-    loss2 = nn.CrossEntropyLoss(reduction="mean")
+    loss1 = nn.MSELoss(reduction='mean')
+    loss2 = nn.CrossEntropyLoss(reduction='mean')
     loss_weights = [0.25, 0.35]
     multimodal_model.compile(optimizer=optim.Adam(multimodal_model.parameters()),
                              loss=[loss1, loss2],
@@ -169,7 +219,7 @@ def test_compute_loss_multimodal(multimodal_model):
 def test_compute_metrics_unimodal(unimodal_model):
     """ Test if the metric computation for a unimodal model matches the expectation. """
     unimodal_model.compile(optimizer=optim.Adam(unimodal_model.parameters()),
-                           loss=nn.CrossEntropyLoss(reduction="mean"),
+                           loss=nn.CrossEntropyLoss(reduction='mean'),
                            metrics=[AccuracyMetric()])
 
     y_pred = torch.randn(10, 5)
@@ -228,7 +278,7 @@ def test_train_step(unimodal_model, mocker):
 def test_test_step(unimodal_model, mocker):
     """ Test if the test step return the expected statistics. """
     unimodal_model.compile(optimizer=optim.Adam(unimodal_model.parameters()),
-                           loss=nn.CrossEntropyLoss(reduction="mean"),
+                           loss=nn.CrossEntropyLoss(reduction='mean'),
                            metrics=[AccuracyMetric()])
 
     x = torch.randn(10, 10)
@@ -253,7 +303,7 @@ def test_fit(unimodal_model, mocker):
     dataloader = DataLoader(dataset, batch_size=len(x))
 
     unimodal_model.compile(optimizer=optim.Adam(unimodal_model.parameters()),
-                           loss=nn.CrossEntropyLoss(reduction="mean"),
+                           loss=nn.CrossEntropyLoss(reduction='mean'),
                            metrics=[AccuracyMetric()])
 
     mocker.patch('alibi.models.pytorch.model.Model.train_step', return_value={'loss': 0., 'accuracy': 1.})
@@ -270,7 +320,7 @@ def test_evaluate(unimodal_model, mocker):
     dataloader = DataLoader(dataset, batch_size=len(x))
 
     unimodal_model.compile(optimizer=optim.Adam(unimodal_model.parameters()),
-                           loss=nn.CrossEntropyLoss(reduction="mean"),
+                           loss=nn.CrossEntropyLoss(reduction='mean'),
                            metrics=[AccuracyMetric()])
 
     mocker.patch('alibi.models.pytorch.model.Model.test_step', return_value={'loss': 0, 'accuracy': 1.})
@@ -291,7 +341,7 @@ def test_metrics_to_str():
 def test_reset_loss_unimodal(unimodal_model):
     """ Test if the model is resetting the loss function in the unimodal case."""
     unimodal_model.compile(optimizer=optim.Adam(unimodal_model.parameters()),
-                           loss=nn.CrossEntropyLoss(reduction="mean"))
+                           loss=nn.CrossEntropyLoss(reduction='mean'))
 
     y_pred = torch.randn(10, 5)
     y_true = torch.randint(low=0, high=5, size=(10, ))
@@ -306,7 +356,7 @@ def test_reset_loss_unimodal(unimodal_model):
 def test_reset_loss_multimodal(multimodal_model):
     """ Test if the model is resetting the loss function in the multimodal case. """
     multimodal_model.compile(optimizer=optim.Adam(multimodal_model.parameters()),
-                             loss=[nn.CrossEntropyLoss(reduction="mean"), nn.CrossEntropyLoss(reduction="mean")],
+                             loss=[nn.CrossEntropyLoss(reduction='mean'), nn.CrossEntropyLoss(reduction='mean')],
                              loss_weights=[1., 1.])
 
     y_pred = [torch.randn(10, 5), torch.randn(10, 5)]

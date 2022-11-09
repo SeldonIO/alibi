091769617f485b1a47f488960145935c7ddb9faf
Robert Samoilescu <robert.samoilescu@gmail.com>
2022-11-09 13:50:33 +0000

    Included test for _plot_pd, _sample_ice and _process_pd_ice. Minor docstring corrections and variable renaming in the pd implementation.

--- alibi/explainers/tests/test_partial_dependence.py
+++ alibi/explainers/tests/test_partial_dependence.py
@@ -5,6 +5,7 @@ from typing import List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import pytest
 from pytest_lazyfixture import lazy_fixture
 from sklearn.datasets import make_classification
@@ -18,13 +19,13 @@ from sklearn.utils import shuffle
 
 from alibi.api.defaults import DEFAULT_DATA_PD, DEFAULT_META_PD
 from alibi.api.interfaces import Explanation
-from alibi.explainers import PartialDependence, TreePartialDependence
+from alibi.explainers import PartialDependence, TreePartialDependence, plot_pd
 from alibi.explainers.partial_dependence import (_plot_one_pd_cat,
                                                  _plot_one_pd_num,
                                                  _plot_two_pd_cat_cat,
                                                  _plot_two_pd_num_cat,
                                                  _plot_two_pd_num_num,
-                                                 _sample_ice)
+                                                 _process_pd_ice, _sample_ice)
 
 
 @pytest.fixture(scope='module')
@@ -149,9 +150,9 @@ def test_explanation_numerical_shapes(rf_classifier, iris_data, grid_resolution,
             assert isinstance(exp.data['feature_values'][i], List)
             assert len(exp.data['feature_values'][i]) == len(f)
             assert len(exp.data['feature_values'][i][0]) == (len(np.unique(X_train[:, f[0]]))
-                                                     if grid_resolution == np.inf else grid_resolution)
+                                                             if grid_resolution == np.inf else grid_resolution)
             assert len(exp.data['feature_values'][i][1]) == (len(np.unique(X_train[:, f[1]]))
-                                                     if grid_resolution == np.inf else grid_resolution)
+                                                             if grid_resolution == np.inf else grid_resolution)
 
             # check pd_values
             assert exp.data['pd_values'][i].shape == (num_targets,
@@ -288,38 +289,6 @@ def test_grid_points_error(adult_data, use_int):
     # compute explanation for every feature using the `grid_points`
     with pytest.raises(ValueError):
         explainer._grid_points_sanity_checks(grid_points, n_features=X_train.shape[1])
-
-
-@pytest.mark.parametrize('n_ice', ['all', 'list', 'int'])
-@pytest.mark.parametrize('n_samples, n_values', [(100, 10)])
-def test_ice_sampling(n_ice, n_samples, n_values):
-    """ Checks if the ice sampling helper function works properly when the arguments are valid. """
-    ice_vals = np.random.randn(n_values, n_samples)
-
-    if n_ice == 'all':
-        ice_sampled_vals = _sample_ice(ice_values=ice_vals, n_ice=n_ice)
-        np.testing.assert_allclose(ice_vals, ice_sampled_vals)
-
-    elif n_ice == 'list':
-        size = np.random.randint(1, n_samples)
-        # needs to be sorted because of the np.unique applied inside the _sample_ice
-        n_ice = np.sort(np.random.choice(n_samples, size=size, replace=False)).tolist()
-        ice_sampled_vals = _sample_ice(ice_values=ice_vals, n_ice=n_ice)
-        np.testing.assert_allclose(ice_vals[:, n_ice], ice_sampled_vals)
-
-    else:
-        n_ice = np.random.randint(1, n_samples)
-        ice_sampled_vals = _sample_ice(ice_values=ice_vals, n_ice=n_ice)
-        assert ice_sampled_vals.shape[1] == n_ice
-
-
-@pytest.mark.parametrize('n_ice', ['unknown', -10, [-1, 1, 2]])
-@pytest.mark.parametrize('n_samples, n_values', [(100, 10)])
-def test_ice_sampling_error(n_samples, n_values, n_ice):
-    """ Checks if the ice sampling helper function throws an error when the arguments are invalid. """
-    ice_vals = np.random.rand(n_values, n_samples)
-    with pytest.raises(ValueError):
-        _sample_ice(ice_values=ice_vals, n_ice=n_ice)
 
 
 @pytest.mark.parametrize('rf_classifier', [lazy_fixture('iris_data')], indirect=True)
@@ -507,34 +476,26 @@ def explanation():
         'grid_resolution': 2,
         'feature_names': ['f_0', 'f_1', 'f_2', 'f_3'],
         'categorical_names': {2: [0, 1, 2], 3: [0, 1, 2, 3, 4]},
-        'target_names': ['c_1', 'c_2']
+        'target_names': ['c_0']
     })
 
     data['data'] = {
         'feature_deciles': [
-            np.array(
-                [-2.81421645, -2.48586831, -2.15752016, -1.82917201, -1.50082386, -1.17247572, -0.84412757, -0.51577942,
-                 -0.18743127, 0.14091688, 0.46926502]),
-            np.array(
-                [-2.06038767, -1.93617523, -1.81196279, -1.68775035, -1.56353791, -1.43932547, -1.31511303, -1.19090059,
-                 -1.06668815, -0.94247571, -0.81826327]),
+            np.array([-1.189, -1.147, -1.104, -1.06, -1.019, -0.977, -0.934, -0.892, -0.849, -0.807, -0.764]),
+            np.array([-0.647, -0.588, -0.528, -0.468, -0.408, -0.348, -0.289, -0.229, -0.169, -0.109, -0.050]),
             None,
             None,
             [
-                np.array([-2.81421645, -2.48586831, -2.15752016, -1.82917201, -1.50082386, -1.17247572, -0.84412757,
-                          -0.51577942, -0.18743127, 0.14091688, 0.46926502]),
-                np.array([-2.06038767, -1.93617523, -1.81196279, -1.68775035, -1.56353791, -1.43932547, -1.31511303,
-                          -1.19090059, -1.06668815, -0.94247571, -0.81826327])
+                np.array([-1.189, -1.147, -1.104, -1.062, -1.019, -0.977, -0.934, -0.892, -0.849, -0.807, -0.764]),
+                np.array([-0.647, -0.588, -0.528, -0.468, -0.408, -0.348, -0.289, -0.229, -0.169, -0.109, -0.050])
             ],
             [
-                np.array([-2.06038767, -1.93617523, -1.81196279, -1.68775035, -1.56353791, -1.43932547, -1.31511303,
-                          -1.19090059, -1.06668815, -0.94247571, -0.81826327]),
+                np.array([-0.647, -0.588, -0.528, -0.468, -0.408, -0.348, -0.289, -0.229, -0.169, -0.109, -0.050]),
                 None
             ],
             [
                 None,
-                np.array([-2.06038767, -1.93617523, -1.81196279, -1.68775035, -1.56353791, -1.43932547, -1.31511303,
-                          -1.19090059, -1.06668815, -0.94247571, -0.81826327])
+                np.array([-0.647, -0.588, -0.528, -0.468, -0.408, -0.348, -0.289, -0.229, -0.169, -0.109, -0.050])
             ],
             [
                 None,
@@ -542,46 +503,45 @@ def explanation():
             ]
         ],
         'pd_values': [
-            np.array([[0.12900344, 0.02424783]]),
-            np.array([[0.00189171, 0.15135956]]),
-            np.array([[0.12759941]]),
-            np.array([[0.12759941]]),
-            np.array([[[0.00329574, 0.25471114], [0.00048768, 0.04800799]]]),
-            np.array([[[0.00189171], [0.15135956]]]),
-            np.array([[[0.00189171, 0.15135956]]]),
-            np.array([[[0.12759941]]])
+            np.array([[0.514, 0.469]]),
+            np.array([[0.213, 0.771]]),
+            np.array([[0.471, 0.515]]),
+            np.array([[0.478, 0.506]]),
+            np.array([[[0.235, 0.792], [0.193, 0.748]]]),
+            np.array([[[0.191, 0.240], [0.742, 0.794]]]),
+            np.array([[[0.191, 0.742], [0.240, 0.794]]]),
+            np.array([[[0.456, 0.482], [0.503, 0.528]]])
         ],
         'ice_values': [
-            np.array([[[0.00329574, 0.00048768], [0.25471114, 0.04800799]]]),
-            np.array([[[0.00048768, 0.04800799], [0.00329574, 0.25471114]]]),
-            np.array([[[0.00048768], [0.25471114]]]),
-            np.array([[[0.00048768], [0.25471114]]]),
-            np.array([[[[0.00329574, 0.25471114], [0.00048768, 0.04800799]],
-                       [[0.00329574, 0.25471114], [0.00048768, 0.04800799]]]]),
-            np.array([[[[0.00048768], [0.04800799]], [[0.00329574], [0.25471114]]]]),
-            np.array([[[[0.00048768, 0.04800799]], [[0.00329574, 0.25471114]]]]),
-            np.array([[[[0.00048768]], [[0.25471114]]]])
+            np.array([[[0.247, 0.203], [0.781, 0.735]]]),
+            np.array([[[0.203, 0.760], [0.223, 0.781]]]),
+            np.array([[[0.160, 0.203], [0.781, 0.827]]]),
+            np.array([[[0.203, 0.230], [0.753, 0.781]]]),
+            np.array([[[[0.247, 0.803], [0.203, 0.760]], [[0.223, 0.781], [0.182, 0.735]]]]),
+            np.array([[[[0.160, 0.203], [0.703, 0.760]], [[0.223, 0.277], [0.781, 0.827]]]]),
+            np.array([[[[0.160, 0.703], [0.203, 0.760]], [[0.223, 0.781], [0.277, 0.827]]]]),
+            np.array([[[[0.160, 0.182], [0.203, 0.230]], [[0.753, 0.781], [0.803, 0.827]]]])
         ],
         'feature_values': [
-            np.array([-2.81421645, 0.46926502]),
-            np.array([-2.06038767, -0.81826327]),
-            np.array([1.]),
-            np.array([3.]),
+            np.array([-1.189, -0.764]),
+            np.array([-0.647, -0.050]),
+            np.array([1., 2.]),
+            np.array([0., 4.]),
             [
-                np.array([-2.81421645, 0.46926502]),
-                np.array([-2.06038767, -0.81826327])
+                np.array([-1.189, -0.764]),
+                np.array([-0.647, -0.050])
             ],
             [
-                np.array([-2.06038767, -0.81826327]),
-                np.array([1.])
+                np.array([-0.647, -0.050]),
+                np.array([1., 2.])
             ],
             [
-                np.array([1.]),
-                np.array([-2.06038767, -0.81826327])
+                np.array([1., 2.]),
+                np.array([-0.647, -0.050])
             ],
             [
-                np.array([1.]),
-                np.array([3.])
+                np.array([1., 2.]),
+                np.array([0., 4.])
             ]
         ],
         'feature_names': [
@@ -827,3 +787,174 @@ def test__plot_two_pd_cat_cat(explanation):
     assert np.allclose(expected_x_ticklabels, x_ticklabels)
     assert np.allclose(expected_y_ticklabels, y_ticklables)
 
+
+def test_plot_pd_all_features(explanation, mocker):
+    """ Test if a PD is plotted for each explained feature. """
+    mocker.patch('alibi.explainers.partial_dependence._plot_one_pd_num', return_value=(None, None))
+    mocker.patch('alibi.explainers.partial_dependence._plot_one_pd_cat', return_value=(None, None))
+    mocker.patch('alibi.explainers.partial_dependence._plot_two_pd_num_num', return_value=(None, None))
+    mocker.patch('alibi.explainers.partial_dependence._plot_two_pd_num_cat', return_value=(None, None))
+    mocker.patch('alibi.explainers.partial_dependence._plot_two_pd_cat_cat', return_value=(None, None))
+    ax = plot_pd(exp=explanation, features='all')
+    assert np.sum(~pd.isna(ax)) == len(explanation.data['feature_names'])
+
+
+def test_plot_pd_oor_feature(explanation):
+    """ Test if an error is raised when the feature index is out of range. """
+    with pytest.raises(IndexError) as err:
+        plot_pd(exp=explanation, features=[len(explanation.data['feature_names'])])
+    assert "The `features` indices must be less than the" in str(err.value)
+
+
+def test_plot_pd_unknown_target(explanation):
+    """ Test if an error is raised for an unknown target name. """
+    with pytest.raises(ValueError) as err:
+        plot_pd(exp=explanation, target="unknown")
+    assert "Unknown `target` name" in str(err.value)
+
+
+def test_plot_pd_oor_target(explanation):
+    """ Test if an error is raised when the target index is out of range. """
+    with pytest.raises(IndexError) as err:
+        plot_pd(exp=explanation, target=len(explanation.meta['params']['target_names']))
+    assert "Target index out of range." in str(err.value)
+
+
+@pytest.mark.parametrize('n_cols', [2, 3, 4, 5])
+def test_plot_pd_n_cols(n_cols, explanation, mocker):
+    """ Test if the number figure columns matches the expected one. """
+    mocker.patch('alibi.explainers.partial_dependence._plot_one_pd_num', return_value=(None, None))
+    mocker.patch('alibi.explainers.partial_dependence._plot_one_pd_cat', return_value=(None, None))
+    mocker.patch('alibi.explainers.partial_dependence._plot_two_pd_num_num', return_value=(None, None))
+    mocker.patch('alibi.explainers.partial_dependence._plot_two_pd_num_cat', return_value=(None, None))
+    mocker.patch('alibi.explainers.partial_dependence._plot_two_pd_cat_cat', return_value=(None, None))
+    ax = plot_pd(exp=explanation, features='all', n_cols=n_cols)
+    assert ax.shape[-1] == n_cols
+
+
+def test_plot_pd_ax(explanation):
+    """ Test if an error is raised if the number of provided axes is less that the number of features to plot
+    the PD for. """
+    _, ax = plt.subplots(nrows=2, ncols=2)
+    with pytest.raises(ValueError) as err:
+        plot_pd(exp=explanation, features='all', ax=ax)
+    assert "Expected ax to have" in str(err.value)
+
+
+def test_plot_pd_sharey_all(explanation):
+    """ Test if all axes have the same y limits when ``sharey='all'``. """
+    explanation.meta['params']['kind'] = 'average'
+    axes = plot_pd(exp=explanation, features=[0, 1, 2, 3], n_cols=1, sharey='all')
+    assert len(set([ax.get_ylim() for ax in axes.ravel()])) == 1
+
+
+@pytest.mark.parametrize('n_cols', [1, 2])
+def test_plot_pd_sharey_row(n_cols, explanation):
+    """ Test if all axes on the same rows have the same y limits and axes on different rows have different y limits
+    when ``sharey='row'``. """
+    features = [0, 1, 2]
+    n_rows = len(features) // n_cols + (len(features) % n_cols != 0)
+    explanation.meta['params']['kind'] = 'average'
+    axes = plot_pd(exp=explanation, features=features, n_cols=n_cols, sharey='row')
+    ylims = []
+
+    for i in range(axes.shape[0]):
+        ylim = set([ax.get_ylim() for ax in axes[i] if ax is not None])
+        assert len(ylim) == 1
+        ylims.append(ylim.pop())
+
+    assert len(set(ylims)) == n_rows
+
+
+@pytest.mark.parametrize('n_cols', [1, 2, 3])
+def test_plot_pd_sharey_none(n_cols, explanation):
+    """Test if all axes have different y limits when ``sharey=None``. """
+    features = [0, 1, 2]
+    explanation.meta['params']['kind'] = 'average'
+    axes = plot_pd(exp=explanation, features=features, n_cols=n_cols, sharey=None)
+    assert len(set([ax.get_ylim() for ax in axes.ravel() if ax is not None])) == len(features)
+
+
+@pytest.mark.parametrize('n_ice', ['all', 'list', 'int'])
+@pytest.mark.parametrize('n_samples, n_values', [(100, 10)])
+def test_ice_sampling(n_ice, n_samples, n_values):
+    """ Checks if the ice sampling helper function works properly when the arguments are valid. """
+    ice_vals = np.random.randn(n_values, n_samples)
+
+    if n_ice == 'all':
+        ice_sampled_vals = _sample_ice(ice_values=ice_vals, n_ice=n_ice)
+        np.testing.assert_allclose(ice_vals, ice_sampled_vals)
+
+    elif n_ice == 'list':
+        size = np.random.randint(1, n_samples)
+        # needs to be sorted because of the np.unique applied inside the _sample_ice
+        n_ice = np.sort(np.random.choice(n_samples, size=size, replace=False)).tolist()
+        ice_sampled_vals = _sample_ice(ice_values=ice_vals, n_ice=n_ice)
+        np.testing.assert_allclose(ice_vals[:, n_ice], ice_sampled_vals)
+
+    else:
+        n_ice = np.random.randint(1, n_samples)
+        ice_sampled_vals = _sample_ice(ice_values=ice_vals, n_ice=n_ice)
+        assert ice_sampled_vals.shape[1] == n_ice
+
+
+def test__sample_ice_warning(explanation, caplog):
+    """ Test if the sampling function logs a warning when the number of ices is greater that the number
+    of instances in the reference dataset and if it returns all ices"""
+    N, V = 10, 5
+    ice_values = np.random.randn(V, N)
+    sampled_ice_values = _sample_ice(ice_values=ice_values, n_ice=N + 1)
+    assert "`n_ice` is greater than the number of instances in the reference dataset." in caplog.records[0].message
+    assert np.allclose(ice_values, sampled_ice_values)
+
+
+def test__sample_ice_error_negative(explanation):
+    """ Test if an error is raise when `n_ice` is negative. """
+    N, V = 10, 5
+    ice_values = np.random.randn(V, N)
+    with pytest.raises(ValueError) as err:
+        _sample_ice(ice_values=ice_values, n_ice=-2)
+    assert "`n_ice` must be an integer grater than 0." == str(err.value)
+
+
+@pytest.mark.parametrize('n_ice', [1, 2, 4, 8])
+def test__sample_ice_error_sample(n_ice, explanation):
+    """ Test if the number of sampled ice matches the expectation. """
+    N, V = 10, 5
+    ice_values = np.random.randn(V, N)
+    sampled_ice_values = _sample_ice(ice_values=ice_values, n_ice=n_ice)
+    assert sampled_ice_values.shape[-1] == n_ice
+
+
+@pytest.mark.parametrize('n_ice', [[-1, 0, 1], [0, 1, 11]])
+def test__sample_ice_error_oor(n_ice, explanation):
+    """ Test if an error is raised when the ice indices are out of bounds. """
+    N, V = 10, 5
+    ice_values = np.random.randn(V, N)
+    with pytest.raises(ValueError) as err:
+        _sample_ice(ice_values=ice_values, n_ice=n_ice)
+    assert "Some indices in `n_ice` are out of bounds." in str(err.value)
+
+
+def test__sample_ice_error_type(explanation):
+    """ Test if an error is raise if the `n_ice` is an unknown type. """
+    N, V = 10, 5
+    ice_values = np.random.randn(V, N)
+    with pytest.raises(ValueError) as err:
+        _sample_ice(ice_values=ice_values, n_ice="unknown")
+    assert "Unknown `n_ice` values." in str(err.value)
+
+
+def test__process_pd_ice(explanation):
+    """ Test the `center` option for the pd and ice. """
+    N, V = 10, 5
+    ice_values = np.random.randn(V, N)
+    pd_values = np.mean(ice_values, axis=-1)
+
+    centered_pd_values, centered_ice_values = _process_pd_ice(exp=explanation,
+                                                              pd_values=pd_values,
+                                                              ice_values=ice_values,
+                                                              n_ice='all',
+                                                              center=True)
+    assert np.isclose(centered_pd_values[0], 0)
+    assert np.allclose(centered_ice_values[0], 0)

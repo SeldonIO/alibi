92f364ee73b5eea99188368e7a73ea1fee3055a5
RobertSamoilescu <robert.samoilescu@gmail.com>
2022-11-17 14:29:15 +0000

    Fix/datasets coverage (#814)
    * Included test for fetching ImageNet10.
    
    * Included Fashion MNIST dataset.
    
    * Included test for ImageNet to check warning message.
    
    * Mean channel shape check for ImageNet10.

--- alibi/datasets/tests/test_datasets.py
+++ alibi/datasets/tests/test_datasets.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pytest
+from alibi.datasets import (fetch_adult, fetch_fashion_mnist, fetch_imagenet,
+                            fetch_imagenet_10, fetch_movie_sentiment,
+                            load_cats)
 from requests import RequestException
-from alibi.datasets import fetch_adult, fetch_movie_sentiment, load_cats
 
 # TODO use mocking instead of relying on external services
 
@@ -63,3 +66,86 @@ def test_cats(return_X_y, target_size):
         y = data.target
     assert len(X) == len(y)
     assert X.shape[1:] == target_size + (3,)  # 3 channels
+
+
+@pytest.mark.parametrize('target_size', [(224, 224)])
+@pytest.mark.parametrize('num_classes', [10])
+def test_imagenet_10(target_size, num_classes):
+    try:
+        data = fetch_imagenet_10()
+    except RequestException:
+        pytest.skip('ImageNet10 dataset URL down.')
+
+    assert isinstance(data, dict)
+
+    # check if the expected keys exist in the dict
+    keys = ['trainset', 'testset', 'int_to_str_labels', 'str_to_int_labels', 'mean_channels']
+    assert all([key in data for key in keys])
+
+    # check the types of the values in the dict
+    assert isinstance(data['trainset'], tuple)
+    assert isinstance(data['testset'], tuple)
+    assert isinstance(data['int_to_str_labels'], dict)
+    assert isinstance(data['str_to_int_labels'], dict)
+    assert isinstance(data['mean_channels'], np.ndarray)
+
+    assert len(data['trainset']) == 2
+    assert len(data['testset']) == 2
+    assert data['mean_channels'].shape[-1] == 3
+
+    X_train, y_train = data['trainset']
+    X_test, y_test = data['testset']
+
+    # check dataset size
+    assert len(X_train) == len(y_train)
+    assert len(X_test) == len(y_test)
+
+    # check input shape
+    assert X_train.shape[1:] == target_size + (3, )
+    assert X_test.shape[1:] == target_size + (3, )
+
+    # check labels
+    train_class_indices = np.unique(y_train)
+    test_class_indices = np.unique(y_test)
+    assert len(train_class_indices) == num_classes
+    assert len(test_class_indices) == num_classes
+
+    class_names = {'stingray', 'trilobite', 'centipede', 'slug', 'snail', 'Rhodesian ridgeback',
+                   'beagle', 'golden retriever', 'sea lion', 'espresso'}
+    train_class_names = set([data['int_to_str_labels'][i] for i in train_class_indices])
+    test_class_names = set([data['int_to_str_labels'][i] for i in test_class_indices])
+    assert train_class_names == class_names
+    assert test_class_names == class_names
+
+
+def test_imagenet():
+    with pytest.warns() as warn:
+        fetch_imagenet()
+    assert 'The Imagenet API is no longer publicly available' in str(warn[0].message)
+
+
+FASHION_MNIST_DIM = 3
+FASHION_MNIST_FEATURES = 784
+FASHION_MNIST_CLASSES = 10
+
+
+@pytest.mark.parametrize('return_X_y', [True, False])
+def test_fashion_mnist(return_X_y):
+
+    try:
+        data = fetch_fashion_mnist(return_X_y=return_X_y)
+    except RequestException:
+        pytest.skip('Fashion MNIST dataset URL down')
+
+    if return_X_y:
+        assert len(data) == 2
+        X, y = data
+    else:
+        assert len(data) == 3
+        X = data.data
+        y = data.target
+
+    assert X.ndim == FASHION_MNIST_DIM
+    assert X.reshape(X.shape[0], -1).shape[1] == FASHION_MNIST_FEATURES
+    assert len(X) == len(y)
+    assert len(set(y)) == FASHION_MNIST_CLASSES

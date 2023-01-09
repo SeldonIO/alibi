a3270be4388164d344b946388c30bffed2903d48
Alex Athorne <alex.athorne@seldon.io>
2023-01-09 15:09:33 +0000

    Update tensorflow _grad_to_numpy type hints

--- alibi/explainers/similarity/backends/tensorflow/base.py
+++ alibi/explainers/similarity/backends/tensorflow/base.py
@@ -55,7 +55,7 @@ class _TensorFlowBackend:
         return grad_X_train
 
     @staticmethod
-    def _grad_to_numpy(grad: tf.Tensor, name: Optional[str] = None) -> tf.Tensor:
+    def _grad_to_numpy(grad: Union[tf.IndexedSlices, tf.Tensor], name: Optional[str] = None) -> tf.Tensor:
         """Convert graidient to numpy array.
 
         Converts gradient tensor to flat numpy array. If the gradient is a sparse tensor, it is converted to a dense

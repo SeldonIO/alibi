0cf22d51cf0c254455ed1a93986b6da382758915
RobertSamoilescu <robert.samoilescu@gmail.com>
2023-02-27 10:32:11 +0000

    Reset predictor within the shap object.

--- alibi/explainers/shap_wrappers.py
+++ alibi/explainers/shap_wrappers.py
@@ -992,8 +992,9 @@ class KernelShap(Explainer, FitMixin):
             New prediction function.
         """
         self.predictor = predictor
-        # TODO: check if we need to reinitialize self._explainer (potentially not, as it should hold a reference
-        #  to self.predictor) however, the shap.KernelExplainer may utilize the Callable to set some attributes
+
+        if self._fitted:
+            self._explainer.model = shap_utils.convert_to_model(self.predictor)
         # TODO: check if we need to do more for the distributed case
 
 
@@ -1628,3 +1629,9 @@ class TreeShap(Explainer, FitMixin):
         """
         # TODO: check what else should be done (e.g. validate dtypes again?)
         self.predictor = predictor
+
+        if self._fitted:
+            self._explainer.model = shap.explainers._tree.TreeEnsemble(predictor,
+                                                                       self._explainer.data,
+                                                                       self._explainer.data_missing,
+                                                                       self._explainer.model_output)

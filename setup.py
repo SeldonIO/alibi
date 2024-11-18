fb8628474984122bb60501aa69d363d44fdc50f1
Robert <robert.samoilescu@gmail.com>
2024-11-18 15:19:13 +0000

    Bumped numpy to 2.x

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
           python_requires='>=3.8',
           # lower bounds based on Debian Stable versions where available
           install_requires=[
-              'numpy>=1.16.2, <2.0.0',
+              'numpy>=2.0.0, <2.1.4',
               'pandas>=1.0.0, <3.0.0',
               'scikit-learn>=1.0.0, <2.0.0',
               'spacy[lookups]>=2.0.0, <4.0.0',
